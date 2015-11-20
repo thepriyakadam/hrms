cd84794e2af3f015d450305d49d91852305d8b25
indiba <it@indiba.in>
2015-11-20 18:41:12 +0530

    drop down

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,3 +31,12 @@ $(function () {
   //Initialize Select2 Elements
   $(".select2").select2();
 });
+
+$(function () {
+  //Datemask dd/mm/yyyy
+  $(".datemask").inputmask("dd/mm/yyyy", {"placeholder": "dd/mm/yyyy"});
+  //Datemask2 mm/dd/yyyy
+  $("#datemask2").inputmask("mm/dd/yyyy", {"placeholder": "mm/dd/yyyy"});
+  //Money Euro
+  $("[data-mask]").inputmask();
+});

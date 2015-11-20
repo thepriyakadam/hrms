ac1e953c91f45095df0eea54e097c73991f87491
indiba <it@indiba.in>
2015-11-20 13:37:20 +0530

    validation of remaning validations

--- app/models/company_location.rb
+++ app/models/company_location.rb
@@ -3,6 +3,31 @@ class CompanyLocation < ActiveRecord::Base
   has_one :member, as: :account
   after_create :create_user_account
 
+
+  validates :name, :presence => true
+  validates :city, :presence => true
+  validates :district , :presence => true
+  validates_length_of :pin_code, is: 6,  message:"Please Enter 6 digit"
+  validates_length_of :contact_no, within: 10..13, message:"Enter the Correct contact number"
+  
+  validates :email, uniqueness: true, :presence => true
+  validate  :email_regex
+  
+  validates :address, :presence => true
+  validate :address_regex
+
+  def address_regex
+    if address.present? and not address.match(/\A[A-Za-z0-9-_]{4,50}\Z/)
+      errors.add :address,"Please Enter The Correct Address"
+    end
+  end
+
+  def email_regex
+    if email.present? and not email.match(/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/)
+      errors.add :email, "This is not a valid email format"
+    end
+   end
+
   def create_user_account
     company_location = CompanyLocation.find(id)
     pass = (0...8).map { (65 + rand(26)).chr}.join
--- app/models/family.rb
+++ app/models/family.rb
@@ -9,7 +9,7 @@ class Family < ActiveRecord::Base
   validates :contact_no, :presence => true
   validates :adhar_no, :presence => true
   validates :relation, :presence => true
-  validates_length_of :pin_code, is: 6,  message:"Please Enter 12 digit"
+  validates_length_of :pin_code, is: 6,  message:"Please Enter 6 digit"
   validates_length_of :adhar_no, is: 12, message:"Please Enter 12 digit"
   validates_length_of :contact_no, within: 10..13, message:"Enter the Correct contact number"
   

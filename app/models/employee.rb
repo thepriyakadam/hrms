d4eb780710289b271fe878f0b27e24d99c634084
indiba <it@indiba.in>
2015-11-21 17:16:25 +0530

    nested form edit problem

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -8,31 +8,31 @@ class Employee < ActiveRecord::Base
   accepts_nested_attributes_for :joining_detail
   after_create :create_user_account
 
-  validates :first_name, :presence => true
-  validates :last_name, :presence => true
-  validates :contact_no, :presence => true
-  validates :adhar_no, :presence => true
-  validates :pan_no, :presence => true
-  validates_length_of :pin_code, is: 6,  message:"Please Enter 6 digit"
-  validates_length_of :adhar_no, is: 12, message:"Please Enter 12 digit"
-  validates_length_of :contact_no, within: 10..13, message:"Enter the Correct contact number"
-  validates :date_of_birth, :presence => true
-  validates :email, uniqueness: true, presence: true
-  validate  :email_regex
-  validates :permanent_address, :presence => true
-  validate :address_regex
+  # validates :first_name, :presence => true
+  # validates :last_name, :presence => true
+  # validates :contact_no, :presence => true
+  # validates :adhar_no, :presence => true
+  # validates :pan_no, :presence => true
+  # validates_length_of :pin_code, is: 6,  message:"Please Enter 6 digit"
+  # validates_length_of :adhar_no, is: 12, message:"Please Enter 12 digit"
+  # validates_length_of :contact_no, within: 10..13, message:"Enter the Correct contact number"
+  # validates :date_of_birth, :presence => true
+  # validates :email, uniqueness: true, presence: true
+  # validate  :email_regex
+  # validates :permanent_address, :presence => true
+  # validate :address_regex
 
-  def address_regex
-    if permanent_address.present? and not permanent_address.match(/\A[A-Za-z0-9-_]{4,50}\Z/)
-      errors.add :permanent_address,"Please Enter The Correct Address"
-    end
-  end
+  # def address_regex
+  #   if permanent_address.present? and not permanent_address.match(/\A[A-Za-z0-9-_]{4,50}\Z/)
+  #     errors.add :permanent_address,"Please Enter The Correct Address"
+  #   end
+  # end
 
-  def email_regex
-    if email.present? and not email.match(/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/)
-      errors.add :email, "This is not a valid email format"
-    end
-   end
+  # def email_regex
+  #   if email.present? and not email.match(/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/)
+  #     errors.add :email, "This is not a valid email format"
+  #   end
+  # end
 
   def create_user_account
     employee = Employee.find(id)

f33d46cc145fe898fe85540f8c5ef1b8015b2234
indiba <it@indiba.in>
2015-11-30 11:00:25 +0530

    updating forms

--- db/migrate/20151116120220_create_departments.rb
+++ db/migrate/20151116120220_create_departments.rb
@@ -2,7 +2,7 @@ class CreateDepartments < ActiveRecord::Migration
   def change
     create_table :departments do |t|
       t.references :company_location, index: true, foreign_key: true
-      t.string :department_code
+      t.string :departement_code
       t.text :description
       t.string :name
       t.references :department_type, index: true, foreign_key: true

ee322de59ee5eca79831391c353231f2210769ff
indiba <it@indiba.in>
2015-12-02 17:43:47 +0530

    update

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,10 +1,12 @@
 class Member < ActiveRecord::Base
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 	#belongs_to :account, polymorphic: true
   belongs_to :company
   belongs_to :company_location
   belongs_to :department
+  belongs_to :role
+  belongs_to :member
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,

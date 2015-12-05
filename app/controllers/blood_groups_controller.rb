18b27b0e10e876606e47876b2f397ba1824146cf
indiba <it@indiba.in>
2015-12-05 15:27:13 +0530

    Ajax JQuery Function

--- app/controllers/blood_groups_controller.rb
+++ app/controllers/blood_groups_controller.rb
@@ -1,65 +1,33 @@
 class BloodGroupsController < ApplicationController
   before_action :set_blood_group, only: [:show, :edit, :update, :destroy]
   load_and_authorize_resource
-  # GET /blood_groups
-  # GET /blood_groups.json
-  def index
-    @blood_groups = BloodGroup.all
-  end
-
-  # GET /blood_groups/1
-  # GET /blood_groups/1.json
-  def show
-  end
-
-  # GET /blood_groups/new
+  
   def new
     @blood_group = BloodGroup.new
+    @blood_groups = BloodGroup.all 
   end
 
-  # GET /blood_groups/1/edit
   def edit
   end
 
-  # POST /blood_groups
-  # POST /blood_groups.json
   def create
     @blood_group = BloodGroup.new(blood_group_params)
-
-    respond_to do |format|
-      if @blood_group.save
-        format.html { redirect_to @blood_group, notice: 'Blood group was successfully created.' }
-        format.json { render :show, status: :created, location: @blood_group }
-      else
-        format.html { render :new }
-        format.json { render json: @blood_group.errors, status: :unprocessable_entity }
-      end
-    end
+    @blood_group.save
+    @blood_groups = BloodGroup.all
+    @blood_group = BloodGroup.new
   end
 
-  # PATCH/PUT /blood_groups/1
-  # PATCH/PUT /blood_groups/1.json
   def update
-    respond_to do |format|
-      if @blood_group.update(blood_group_params)
-        format.html { redirect_to @blood_group, notice: 'Blood group was successfully updated.' }
-        format.json { render :show, status: :ok, location: @blood_group }
-      else
-        format.html { render :edit }
-        format.json { render json: @blood_group.errors, status: :unprocessable_entity }
-      end
-    end
+    @blood_group.update(blood_group_params)
+    @blood_groups = BloodGroup.all
+    @blood_group = BloodGroup.new
   end
 
-  # DELETE /blood_groups/1
-  # DELETE /blood_groups/1.json
   def destroy
     @blood_group.destroy
-    respond_to do |format|
-      format.html { redirect_to blood_groups_url, notice: 'Blood group was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    @blood_groups = BloodGroup.all
   end
+ 
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- app/views/leav_categories/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@leav_categories) do |leav_category|
-  json.extract! leav_category, :id, :name
-  json.url leav_category_url(leav_category, format: :json)
-end

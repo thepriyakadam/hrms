a301480121de700ad13a616886d3395a28c8555d
vh <it@indiba.in>
2015-12-16 17:03:42 +0530

    Changes

--- app/models/skillset.rb
+++ app/models/skillset.rb
@@ -2,13 +2,5 @@ class Skillset < ActiveRecord::Base
   belongs_to :employee
 
 
-  validates :name, :presence => true 
-  validate :skillset_regex
-
-  def skillset_regex
-    if name.present? and not name.match(/\A[A-Za-z_]{1,30}\Z/)
-      errors.add :name,"Name allows only Characters"
-    end
-  end
-
+  validates :name, :presence => true ,uniqueness: true
 end

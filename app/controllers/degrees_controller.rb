1c1ca38720051c151de94a66f45928fc9214b3fe
indiba <it@indiba.in>
2015-12-18 15:38:45 +0530

    changes

--- app/controllers/degrees_controller.rb
+++ app/controllers/degrees_controller.rb
@@ -15,10 +15,15 @@ class DegreesController < ApplicationController
   # POST /degrees.json
   def create
     @degree = Degree.new(degree_params)
-    @degree.save
     @degrees = Degree.all
+     respond_to do |format|
+    if @degree.save
     @degree = Degree.new
-       
+    format.js { @flag = true }
+    else
+      flash.now[:alert] = "Blood Group Already Exist."
+      format.js { @flag = false }
+    end    
   end
 
   # PATCH/PUT /degrees/1

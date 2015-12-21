72192b367828afb63521f06f9ab055d59768aedd
vh <it@indiba.in>
2015-12-22 00:25:58 +0530

    religion mastre

--- app/controllers/religions_controller.rb
+++ app/controllers/religions_controller.rb
@@ -1,20 +1,10 @@
 class ReligionsController < ApplicationController
   before_action :set_religion, only: [:show, :edit, :update, :destroy]
 
-  # GET /religions
-  # GET /religions.json
-  def index
-    @religions = Religion.all
-  end
-
-  # GET /religions/1
-  # GET /religions/1.json
-  def show
-  end
-
   # GET /religions/new
   def new
     @religion = Religion.new
+    @religions = Religion.all
   end
 
   # GET /religions/1/edit
@@ -25,14 +15,14 @@ class ReligionsController < ApplicationController
   # POST /religions.json
   def create
     @religion = Religion.new(religion_params)
-
+    @religions = Religion.all
     respond_to do |format|
       if @religion.save
-        format.html { redirect_to @religion, notice: 'Religion was successfully created.' }
-        format.json { render :show, status: :created, location: @religion }
+         @religion = Religion.new
+          format.js { @flag = true }
       else
-        format.html { render :new }
-        format.json { render json: @religion.errors, status: :unprocessable_entity }
+        flash.now[:alert] = "Religion Already Exist."
+         format.js { @flag = false }
       end
     end
   end
@@ -40,25 +30,16 @@ class ReligionsController < ApplicationController
   # PATCH/PUT /religions/1
   # PATCH/PUT /religions/1.json
   def update
-    respond_to do |format|
-      if @religion.update(religion_params)
-        format.html { redirect_to @religion, notice: 'Religion was successfully updated.' }
-        format.json { render :show, status: :ok, location: @religion }
-      else
-        format.html { render :edit }
-        format.json { render json: @religion.errors, status: :unprocessable_entity }
-      end
-    end
+      @religion.update(religion_params)
+      @religions = Religion.all
+      @religion = Religion.new
   end
 
   # DELETE /religions/1
   # DELETE /religions/1.json
   def destroy
     @religion.destroy
-    respond_to do |format|
-      format.html { redirect_to religions_url, notice: 'Religion was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    @religions = Religion.all
   end
 
   private
--- app/views/religions/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@religions) do |religion|
-  json.extract! religion, :id, :name, :code, :description
-  json.url religion_url(religion, format: :json)
-end

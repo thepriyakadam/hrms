b4cbe4b023d6157b8fab8a25d683c90658f66c73
indiba <it@indiba.in>
2015-12-22 16:44:51 +0530

    change

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -1,20 +1,11 @@
 class BanksController < ApplicationController
   before_action :set_bank, only: [:show, :edit, :update, :destroy]
 
-  # GET /banks
-  # GET /banks.json
-  def index
-    @banks = Bank.all
-  end
-
-  # GET /banks/1
-  # GET /banks/1.json
-  def show
-  end
 
   # GET /banks/new
   def new
     @bank = Bank.new
+    @banks = Bank.all
   end
 
   # GET /banks/1/edit
@@ -25,14 +16,14 @@ class BanksController < ApplicationController
   # POST /banks.json
   def create
     @bank = Bank.new(bank_params)
-
+    @banks = Bank.all
     respond_to do |format|
       if @bank.save
-        format.html { redirect_to @bank, notice: 'Bank was successfully created.' }
-        format.json { render :show, status: :created, location: @bank }
+         @bank = Bank.new
+        format.js { @flag = true }
       else
-        format.html { render :new }
-        format.json { render json: @bank.errors, status: :unprocessable_entity }
+        flash.now[:alert] = "Bank Already Exist."
+         format.js { @flag = false }
       end
     end
   end
@@ -40,25 +31,16 @@ class BanksController < ApplicationController
   # PATCH/PUT /banks/1
   # PATCH/PUT /banks/1.json
   def update
-    respond_to do |format|
-      if @bank.update(bank_params)
-        format.html { redirect_to @bank, notice: 'Bank was successfully updated.' }
-        format.json { render :show, status: :ok, location: @bank }
-      else
-        format.html { render :edit }
-        format.json { render json: @bank.errors, status: :unprocessable_entity }
-      end
-    end
+      @bank.update(bank_params)
+      @banks = Bank.all
+      @bank = Bank.new
   end
 
   # DELETE /banks/1
   # DELETE /banks/1.json
   def destroy
     @bank.destroy
-    respond_to do |format|
-      format.html { redirect_to banks_url, notice: 'Bank was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    @banks = Bank.all
   end
 
   private
--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,2 +1,3 @@
 class Bank < ActiveRecord::Base
+	validates :name, :presence => true,  uniqueness: { case_sensitive: false }
 end

9b48463fd388294f88d9468bb7ccbca2dd002ba2
Ravikiran <ravikirankadam.1991@gmail.com>
2015-12-22 16:52:53 +0530

    puhs payroll dynamic form

--- app/controllers/employee_annual_salaries_controller.rb
+++ app/controllers/employee_annual_salaries_controller.rb
@@ -5,22 +5,45 @@ class EmployeeAnnualSalariesController < ApplicationController
   end
 
   def create
-  	p "------------------------------------------------"
-  	@item = params["employee_annual_salary_data"]
+  	@items = params["employee_annual_salary_data"]
   	@employee_ids = params["employee_ids"]
-  	ActiveRecord::Base.transaction do
-	  	@employee_ids.each do |id|
-		  	@item.each do |k,v|
-		  		EmployeeAnnualSalary.new do |e|
-		  			e.employee_id = id
-		  			e.salary_component_id = k
-		  			e.amount = v
-		  			e.save
-		  		end
-		  	end
-		  end
-		end  	
-  	flash[:alert] = "This is under construction."
-  	redirect_to employee_annual_salaries_path
+    
+    flag = false
+    @items.each do |k,v|
+      if v == ""
+        flag = true
+      end
+    end
+
+    if flag
+      @salary_components = SalaryComponent.all
+      @employees = Employee.all
+      flash.now[:alert] = "All fields are mandotory."
+      render :index
+    elsif @employee_ids.nil?
+      @salary_components = SalaryComponent.all
+      @employees = Employee.all
+      flash.now[:alert] = "Please select employee."
+      render :index
+    else
+      p "data savedddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
+      redirect_to employee_annual_salaries_path
+    end
+
+    
+  # 	ActiveRecord::Base.transaction do
+	 #  	@employee_ids.try(:each) do |id|
+		#   	@item.each do |k,v|
+		#   		EmployeeAnnualSalary.new do |e|
+		#   			e.employee_id = id
+		#   			e.salary_component_id = k
+		#   			e.amount = v
+		#   			e.save
+		#   		end
+		#   	end
+		#   end
+		# end  	
+  # 	flash[:alert] = "This is under construction."
+  # 	redirect_to employee_annual_salaries_path
   end
 end

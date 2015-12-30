ea76c6a3fd52de6e5c6a81c12bbd911dfa8d64fa
indiba <it@indiba.in>
2015-12-30 16:33:19 +0530

    updated Changes

--- app/models/employee_shift.rb
+++ app/models/employee_shift.rb
@@ -2,5 +2,10 @@ class EmployeeShift < ActiveRecord::Base
   belongs_to :company_shift
   belongs_to :employee
 
-  validates :company_shift, :presence => true
+  validates :company_shift_id, :presence => true
+  validates :from_date, :presence => true
+  validates :to_date, :presence => true
+
+
+
 end

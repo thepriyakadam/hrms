fcae6d3b158eb4d7cf2f56a5ee8c550024a55628
vh <it@indiba.in>
2015-12-31 09:25:50 +0530

    update salary_template

--- app/controllers/salary_component_templates_controller.rb
+++ app/controllers/salary_component_templates_controller.rb
@@ -11,6 +11,12 @@ class SalaryComponentTemplatesController < ApplicationController
     @salary_template = SalaryTemplate.find(params[:s])
   end
 
+   def edit
+    @salary_component_template = SalaryComponentTemplate.find(params[:id])
+    @form = 'salary_component_template'
+    @salary_template = @salary_component_template.salary_template
+  end
+
   def create
     @salary_component_template = SalaryComponentTemplate.new(salary_component_template_params) 
     if @salary_component_template.save
@@ -20,6 +26,23 @@ class SalaryComponentTemplatesController < ApplicationController
     else
       flash[:alert] = "Salary Data Not Added Successfully."
       @flag = false
+    end
+  end
+
+  def update
+    @salary_component_template = SalaryComponentTemplate.find(params[:id])
+    @salary_component_templates = SalaryComponentTemplate.where(salary_template_id: @salary_component_template.salary_template_id)
+    respond_to do |format|
+      if @salary_component_template.update(salary_component_template_params)
+        #format.html { redirect_to @family, notice: 'Family was successfully updated.' }
+        #format.json { render :show, status: :ok, location: @family }
+        # @salary_component_templates = @salary_template.salary_component_templates
+        format.js { @flag = true }
+      else
+        #format.html { render :edit }
+        #format.json { render json: @family.errors, status: :unprocessable_entity }
+        format.js { @flag = false }
+      end
     end
   end
 

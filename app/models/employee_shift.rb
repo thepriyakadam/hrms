30bd67e35639dcfa55cc7e59b6dbaf75f2ca2907
Ravikiran <ravikirankadam.1991@gmail.com>
2016-01-07 15:26:35 +0530

    push code

--- app/models/employee_shift.rb
+++ app/models/employee_shift.rb
@@ -2,7 +2,7 @@ class EmployeeShift < ActiveRecord::Base
   #belongs_to :company_shift
   belongs_to :employee
   has_and_belongs_to_many :shift_rotations
-  validates :company_shift_id, :presence => true
-  validates :from_date, :presence => true
-  validates :to_date, :presence => true
+  # validates :company_shift_id, :presence => true
+  # validates :from_date, :presence => true
+  # validates :to_date, :presence => true
 end

e7d30fd50d4566ee38a661092d348288d88cbfdd
vh <it@indiba.in>
2016-01-27 18:18:56 +0530

    push code ESIC Master

---
+++ db/migrate/20160127112049_create_esic_masters.rb
@@ -0,0 +1,13 @@
+class CreateEsicMasters < ActiveRecord::Migration
+  def change
+    create_table :esic_masters do |t|
+      t.boolean :esic
+      t.decimal :percentage, :decimal, precision: 15, scale: 2, default: 0
+      t.date :date_effective
+      t.decimal :max_limit, :decimal, precision: 15, scale: 2, default: 0
+      t.string :base_component
+
+      t.timestamps null: false
+    end
+  end
+end

3fe35fe0d0f2a9e86646751028bcb98bc4e520b1
vh <it@indiba.in>
2016-01-28 16:49:01 +0530

    push

--- app/models/advance_salary.rb
+++ app/models/advance_salary.rb
@@ -1,4 +1,5 @@
 class AdvanceSalary < ActiveRecord::Base
   belongs_to :employee
   has_many :instalments
+  
 end
--- app/models/salaryslip.rb
+++ app/models/salaryslip.rb
@@ -3,4 +3,15 @@ class Salaryslip < ActiveRecord::Base
   belongs_to :template
   belongs_to :workingday
   belongs_to :salary_template
+
+def self.to_csv(options = {})
+  CSV.generate(options) do |csv|
+    csv << column_names
+    all.each do |salaryslip|
+      csv << salaryslip.attributes.values_at(*column_names)
+    end
+  end
+end
+
+
 end

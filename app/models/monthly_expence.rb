ea0437aab7f4216ab6a57baecafcaec3d7818dc0
HR <it@indiba.in>
2016-03-03 16:14:42 +0530

    pull code

--- app/models/monthly_expence.rb
+++ app/models/monthly_expence.rb
@@ -2,4 +2,12 @@ class MonthlyExpence < ActiveRecord::Base
   belongs_to :employee
   belongs_to :expencess_type
   attr_accessor :total_amount
+
+  validates :expence_date, :presence => true
+  validates :amount, :presence => true
+  validates :expence_type_id, :presence => true
+  validates :employee_id, :presence => true
+
+
+
 end

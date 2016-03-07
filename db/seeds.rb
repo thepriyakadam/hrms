f88fcc7a316eee710d2088f1eb0c21be240ba968
HR <it@indiba.in>
2016-03-07 19:54:11 +0530

    push code

--- db/seeds.rb
+++ db/seeds.rb
@@ -283,41 +283,42 @@ require 'roo'
 #   end
 # end
 # end
-ex = Roo::Excel.new("#{Rails.root}/public/workingdays.xls")
-ex.default_sheet = ex.sheets[7]
-i = 1
-gross_salary = 0
-ActiveRecord::Base.transaction do
-#2.upto(372) do |line| # dewas jan 2016
-#2.upto(97) do |line| # siya jan 2016
-2.upto(95) do |line| # siya Feb 2016
-  puts "Starting Record #{ex.cell(line,'A')}---------------------------------------"
-  @employee = Employee.find_by_manual_employee_code(ex.cell(line,'A').to_i)
-  unless @employee.nil?
-    Workingday.new do |w|
-      w.employee_id = @employee.id
-      w.month_name = ex.cell(line,'B')
-      w.year = ex.cell(line,'C').to_i
+#####################################################################
+# ex = Roo::Excel.new("#{Rails.root}/public/workingdays.xls")
+# ex.default_sheet = ex.sheets[7]
+# i = 1
+# gross_salary = 0
+# ActiveRecord::Base.transaction do
+# #2.upto(372) do |line| # dewas jan 2016
+# #2.upto(97) do |line| # siya jan 2016
+# 2.upto(95) do |line| # siya Feb 2016
+#   puts "Starting Record #{ex.cell(line,'A')}---------------------------------------"
+#   @employee = Employee.find_by_manual_employee_code(ex.cell(line,'A').to_i)
+#   unless @employee.nil?
+#     Workingday.new do |w|
+#       w.employee_id = @employee.id
+#       w.month_name = ex.cell(line,'B')
+#       w.year = ex.cell(line,'C').to_i
 
-      w.lwp_leave = ex.cell(line, 'D').to_i
-      w.cl_leave = ex.cell(line, 'E').to_i
-      w.el_leave = ex.cell(line, 'F').to_i
-      w.esic_leave = ex.cell(line, 'G').to_i
+#       w.lwp_leave = ex.cell(line, 'D').to_i
+#       w.cl_leave = ex.cell(line, 'E').to_i
+#       w.el_leave = ex.cell(line, 'F').to_i
+#       w.esic_leave = ex.cell(line, 'G').to_i
 
-      w.day_in_month = ex.cell(line, 'H')
-      w.present_day = ex.cell(line, 'I')
-      w.holiday_in_month = ex.cell(line, 'J')
+#       w.day_in_month = ex.cell(line, 'H')
+#       w.present_day = ex.cell(line, 'I')
+#       w.holiday_in_month = ex.cell(line, 'J')
        
-      w.week_off_day = ex.cell(line, 'K')
-      w.absent_day = ex.cell(line, 'L')
-      w.payable_day = ex.cell(line, 'M')
-      w.save!
-    end
-    puts "#{i} Record inserted.-----------------------------------------------"
-    i = i+1
-  end
-end
-end
+#       w.week_off_day = ex.cell(line, 'K')
+#       w.absent_day = ex.cell(line, 'L')
+#       w.payable_day = ex.cell(line, 'M')
+#       w.save!
+#     end
+#     puts "#{i} Record inserted.-----------------------------------------------"
+#     i = i+1
+#   end
+# end
+# end
 
 ###############################################################################################
 
@@ -716,51 +717,45 @@ end
 # end
 ######################################################################################3
 @elrs = EmployeeLeavRequest.where(current_status: "Pending")
+ActiveRecord::Base.transaction do
 @elrs.each do |r|
   unless r.employee.manager_id.nil?
     if r.employee.manager_2_id.nil?
-      @elrs = EmployeeLeavRequest.where(employee_id: e.id)
-      ActiveRecord::Base.transaction do
-          r.is_first_approved = true
-          r.current_status = "FirstApproved"
-          #r.update(is_first_approved: true, current_status: 'FirstApproved')
-          r.leave_status_records.build(change_status_employee_id: r.first_reporter_id,status: 'FirstApproved',change_date:'01/01/2016')
-          if r.leave_type == "Full Day"
-            for i in r.start_date.to_date..r.end_date.to_date
-              r.particular_leave_records.build(employee_id: r.employee_id, leave_date: i ,is_full: true )
-            end
-          else
-            r.particular_leave_records.build(employee_id: r.employee_id, leave_date: r.start_date ,is_full: false )
-          end
-          r.save
-          r.minus_leave(r)
-          p "First Approved only for single layer employee"
-      end
-    else
-      ActiveRecord::Base.transaction do
-          r.is_first_approved = true
-          r.current_status = "FirstApproved"
-          r.second_reporter_id = r.employee.manager_2_id
-          r.leave_status_records.build(change_status_employee_id: r.first_reporter_id,status: 'FirstApproved',change_date:'01/02/2016')
-          r.save
-          p "First Approved only for double layer employee"
-
-        @elrs.each do |r|
-          r.is_second_approved = true
-          r.current_status = "SecondApproved"
-          r.leave_status_records.build(change_status_employee_id: r.second_reporter_id,status: 'SecondApproved',change_date:'01/02/2016')
-          if r.leave_type == "Full Day"
-            for i in r.start_date.to_date..r.end_date.to_date
-              r.particular_leave_records.build(employee_id: r.employee_id, leave_date: i ,is_full: true )
-            end
-          else
-            r.particular_leave_records.build(employee_id: r.employee_id, leave_date: r.start_date ,is_full: false )
-          end
-          r.save
-          r.minus_leave(r)
-          p "Second Approved only for double layer employee"
+      r.is_first_approved = true
+      r.current_status = "FirstApproved"
+      r.leave_status_records.build(change_status_employee_id: r.first_reporter_id,status: 'FirstApproved',change_date:'01/01/2016')
+      if r.leave_type == "Full Day"
+        for i in r.start_date.to_date..r.end_date.to_date
+          r.particular_leave_records.build(employee_id: r.employee_id, leave_date: i ,is_full: true )
         end
+      else
+        r.particular_leave_records.build(employee_id: r.employee_id, leave_date: r.start_date ,is_full: false )
       end
+      r.save
+      r.minus_leave(r)
+      p "First Approved only for single layer employee"
+    else
+      r.is_first_approved = true
+      r.current_status = "FirstApproved"
+      r.second_reporter_id = r.employee.manager_2_id
+      r.leave_status_records.build(change_status_employee_id: r.first_reporter_id,status: 'FirstApproved',change_date:'01/02/2016')
+      r.save
+      p "First Approved only for double layer employee"
+
+      r.is_second_approved = true
+      r.current_status = "SecondApproved"
+      r.leave_status_records.build(change_status_employee_id: r.second_reporter_id,status: 'SecondApproved',change_date:'01/02/2016')
+      if r.leave_type == "Full Day"
+        for i in r.start_date.to_date..r.end_date.to_date
+          r.particular_leave_records.build(employee_id: r.employee_id, leave_date: i ,is_full: true )
+        end
+      else
+        r.particular_leave_records.build(employee_id: r.employee_id, leave_date: r.start_date ,is_full: false )
+      end
+      r.save
+      r.minus_leave(r)
+      p "Second Approved only for double layer employee"
     end
   end
+end
 end

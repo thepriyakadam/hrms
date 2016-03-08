a1dbaa33c17cf1b3af889607090e20edc67459d1
Ravikiran <ravikirankadam.1991@gmail.com>
2016-03-08 18:51:42 +0530

    pull code

--- app/models/overtime_salary.rb
+++ app/models/overtime_salary.rb
@@ -1,3 +1,12 @@
 class OvertimeSalary < ActiveRecord::Base
   belongs_to :employee
+  before_create :calculate_amount
+  #after_update :update_employee_leave_balance
+
+  def calculate_amount
+    @overtime_master = OvertimeMaster.find_by_status(true)
+    temp = (6500/@overtime_master.day/@overtime_master.company_hrs*@overtime_master.ot_rate*self.ot_hrs)
+    puts temp
+    puts "--------------------------------------------------------------------------"
+  end
 end

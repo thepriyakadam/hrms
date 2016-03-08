f167cb7fb11885fe87e099f96a2d0fa73b56e695
vh <it@indiba.in>
2016-03-08 18:52:59 +0530

    push code

--- app/controllers/bonus_masters_controller.rb
+++ app/controllers/bonus_masters_controller.rb
@@ -1,20 +1,11 @@
 class BonusMastersController < ApplicationController
   before_action :set_bonus_master, only: [:show, :edit, :update, :destroy]
 
-  # GET /bonus_masters
-  # GET /bonus_masters.json
-  def index
-    @bonus_masters = BonusMaster.all
-  end
-
-  # GET /bonus_masters/1
-  # GET /bonus_masters/1.json
-  def show
-  end
-
+  
   # GET /bonus_masters/new
   def new
     @bonus_master = BonusMaster.new
+    @bonus_masters = BonusMaster.all
   end
 
   # GET /bonus_masters/1/edit
@@ -25,40 +16,32 @@ class BonusMastersController < ApplicationController
   # POST /bonus_masters.json
   def create
     @bonus_master = BonusMaster.new(bonus_master_params)
-
+    @bonus_masters = BonusMaster.all
     respond_to do |format|
       if @bonus_master.save
-        format.html { redirect_to @bonus_master, notice: 'Bonus master was successfully created.' }
-        format.json { render :show, status: :created, location: @bonus_master }
+        @bonus_master = BonusMaster.new
+         format.js { @flag = true }
       else
-        format.html { render :new }
-        format.json { render json: @bonus_master.errors, status: :unprocessable_entity }
+        flash.now[:alert] = "Bonus Master Already Exist."
+        format.js { @flag = false }
       end
     end
+        
   end
 
   # PATCH/PUT /bonus_masters/1
   # PATCH/PUT /bonus_masters/1.json
   def update
-    respond_to do |format|
-      if @bonus_master.update(bonus_master_params)
-        format.html { redirect_to @bonus_master, notice: 'Bonus master was successfully updated.' }
-        format.json { render :show, status: :ok, location: @bonus_master }
-      else
-        format.html { render :edit }
-        format.json { render json: @bonus_master.errors, status: :unprocessable_entity }
-      end
-    end
+    @bonus_master.update(bonus_master_params)
+    @bonus_masters = BonusMaster.all
+    @bonus_master = BonusMaster.new
   end
 
   # DELETE /bonus_masters/1
   # DELETE /bonus_masters/1.json
   def destroy
     @bonus_master.destroy
-    respond_to do |format|
-      format.html { redirect_to bonus_masters_url, notice: 'Bonus master was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    @bonus_masters = BonusMaster.all
   end
 
   private

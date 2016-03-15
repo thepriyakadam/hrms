2aec0872b167757176f1b1abf96b446aa2013306
indiba <it@indiba.in>
2016-03-15 17:05:36 +0530

    Change View Pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160309115846) do
+ActiveRecord::Schema.define(version: 20160312115859) do
 
   create_table "advance_salaries", force: :cascade do |t|
     t.integer  "employee_id"
@@ -80,16 +80,6 @@ ActiveRecord::Schema.define(version: 20160309115846) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "bonus", force: :cascade do |t|
-    t.integer  "employee_id"
-    t.decimal  "bonus_amount", precision: 15, scale: 2, default: 0.0
-    t.string   "bouns_date"
-    t.datetime "created_at",                                          null: false
-    t.datetime "updated_at",                                          null: false
-  end
-
-  add_index "bonus", ["employee_id"], name: "index_bonus_on_employee_id"
 
   create_table "bonus_employees", force: :cascade do |t|
     t.integer  "employee_id"
@@ -886,6 +876,17 @@ ActiveRecord::Schema.define(version: 20160309115846) do
     t.datetime "created_at",                                          null: false
     t.datetime "updated_at",                                          null: false
   end
+
+  create_table "overtime_month_records", force: :cascade do |t|
+    t.integer  "employee_id"
+    t.date     "overtime_data"
+    t.decimal  "attendance_bonus_amount", precision: 15, scale: 2
+    t.decimal  "paid_holiday_amount",     precision: 15, scale: 2
+    t.datetime "created_at",                                       null: false
+    t.datetime "updated_at",                                       null: false
+  end
+
+  add_index "overtime_month_records", ["employee_id"], name: "index_overtime_month_records_on_employee_id"
 
   create_table "overtime_salaries", force: :cascade do |t|
     t.integer  "employee_id"

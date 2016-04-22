3b5226c963f95699619e61805afcc14e6f87b0ed
vh <it@indiba.in>
2016-04-22 11:54:38 +0530

    pull code

--- app/helpers/vacancy_masters_helper.rb
+++ app/helpers/vacancy_masters_helper.rb
@@ -39,16 +39,26 @@ module VacancyMastersHelper
     end
   end
 
-  def all_department_type_list2
-    if current_user.class == Group
-      Department.all.collect { |d| [d.name, d.id] }
+  def role_location_list
+   if current_user.class == Group
+      CompanyLocation.all.collect { |d| [d.company_location.company.name + '-' + d.name , d.id] }
     else
       if current_user.role.name == 'Company'
-        Department.all.collect { |d| [d.name, d.id] }
+        CompanyLocation.all.collect { |d| [d.company_location.company.name + '-' + d.name, d.id] }
       elsif current_user.role.name == 'CompanyLocation'
-        Department.where(company_location_id: current_user.company_location_id).collect { |d| [d.name, d.id] }
-      elsif current_user.role.name == 'Department'
-        Department.where(id: current_user.department_id).collect { |d| [d.name, d.id] }
+        CompanyLocation.where(id: current_user.company_location_id).collect { |d| [d.company.name + '-' + d.name, d.id] }
+      end
+    end
+  end
+
+  def all_department_type_list2
+     if current_user.class == Group
+      Department.all.collect { |d| [d.company_location.company.name + '-' + d.company_location.name + '-' + d.name, d.id] }
+    else
+      if current_user.role.name == 'Company'
+        Department.all.collect { |d| [d.company_location.company.name + '-' + d.company_location.name + '-' + d.name, d.id] }
+      elsif current_user.role.name == 'CompanyLocation'
+        Department.where(company_location_id: current_user.company_location_id).collect { |d| [d.company_location.company.name + '-' + d.company_location.name + '-' + d.name, d.id] }
       end
     end
   end

03f32d699b3a2a8233e96ba4ee75d29164951e78
indiba <it@indiba.in>
2016-04-28 16:12:55 +0530

    work on email feature

--- app/mailers/vacancy_master_mailer.rb
+++ app/mailers/vacancy_master_mailer.rb
@@ -3,26 +3,26 @@ class VacancyMasterMailer < ApplicationMailer
   def vacancy_request(vacancy_master)
      # @vacancy_master = VacancyMaster.find_by_reporting_master_id(vacancy_master.reporting_master_id)
      @employee = Employee.find(vacancy_master.reporting_master_id)
-     @vacancy_master = VacancyMaster.find_by_reporting_master_id(vacancy_master.reporting_master_id)
+     @vacancy_master = VacancyMaster.find(vacancy_master.id)
      mail(to: @employee.email, subject: 'Vacancy Request')
   end
 
   def approve_vacancy_email(vacancy_master)
-  	 @employee = Employee.find(vacancy_master.reporting_master_id)
+  	 @employee = Employee.find(vacancy_master.employee_id)
      # @employee = VacancyMaster.find(vacancy_master.reporting_master_id)
-     @vacancy_master = VacancyMaster.find_by_reporting_master_id(vacancy_master.reporting_master_id)
-     mail(to: @employee.email, subject: 'Vacancy Approve Request')
+     @vacancy_master = VacancyMaster.find(vacancy_master.employee_id)
+     mail(to: @vacancy_master.email, subject: 'Vacancy Approve Request')
   end
 
   def reject_vacancy_email(vacancy_master)
   	 @employee = Employee.find(vacancy_master.reporting_master_id)
-     @vacancy_master = VacancyMaster.find_by_reporting_master_id(vacancy_master.reporting_master_id)
+     @vacancy_master = VacancyMaster.find(vacancy_master.id)
      mail(to: @employee.email, subject: 'Vacancy Reject Request')
   end
 
    def cancel_vacancy_email(vacancy_master)
      @employee = Employee.find(vacancy_master.reporting_master_id)
-     @vacancy_master = VacancyMaster.find_by_reporting_master_id(vacancy_master.reporting_master_id)
+     @vacancy_master = VacancyMaster.find(vacancy_master.id)
      mail(to: @employee.email, subject: 'Vacancy Cancel Request')
   end
 

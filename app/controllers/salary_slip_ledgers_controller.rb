14d16f3a268f6839c97673280029d664e177a531
indiba <it@indiba.in>
2016-05-02 18:31:30 +0530

    salaryslip ledger report

--- app/controllers/salary_slip_ledgers_controller.rb
+++ app/controllers/salary_slip_ledgers_controller.rb
@@ -1,16 +1,7 @@
 class SalarySlipLedgersController < ApplicationController
 
     def show_employee
-      @month = params[:month]
-      @year = params[:year]
-      # @employees = params[:employee_ids]
-      
-      @employees= Employee.all
-      # bank_name = params[:bank_name]
-      # @salaryslips = Salaryslip.where(month: @month, year: @year.to_s).pluck(:employee_id)
-      # @employees = EmployeeBankDetail.where(id: @salaryslips, bank_name: bank_name)
-
-      # @employee_bank_details = EmployeeBankDetail.where(id: @salaryslips, employee_id: bank_name)
+     @salaryslip_components = SalaryslipComponent.all
     end
 
     # def show_employee

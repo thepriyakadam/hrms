a5fe97c6ee42c222435a9341281d01b37302c7b7
indiba <it@indiba.in>
2016-05-09 16:01:44 +0530

    goal

--- app/models/selected_resume.rb
+++ app/models/selected_resume.rb
@@ -1,4 +1,11 @@
 class SelectedResume < ActiveRecord::Base
   belongs_to :degree
   belongs_to :vacancy_master
+
+  has_attached_file :avatar, path: ':rails_root/attachments_resume/:id/:style/:basename.:extension' 
+  has_attached_file :passport_photo, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: 'missing.png'
+  validates_attachment_content_type :passport_photo, content_type: /\Aimage\/.*\Z/
+  # validates_attachment_presence :avatar
+  validates_attachment_size :avatar, less_than: 2.megabytes
+  validates_attachment :avatar, content_type: { content_type: %w(application/pdf application/msword application/msexcel application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -316,6 +316,8 @@ Rails.application.routes.draw do
       get :show_leave_record
     end
   end
+  match 'selected_resumes/:id/download_resume/:id' => 'selected_resumes#download_resume', :via => [:get], :as => :download_resume
+  match 'selected_resumes/:id/download_image/:id' => 'selected_resumes#download_image', :via => [:get], :as => :download_image
 
   match 'capture_resumes/:id/download/:id' => 'capture_resumes#download', :via => [:get], :as => :download
   match 'capture_resumes/:id/download_photo/:id' => 'capture_resumes#download_photo', :via => [:get], :as => :download_photo

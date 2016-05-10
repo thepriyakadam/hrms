a63cf7ca0bca5e22f10865eb43f88cfbe4ffa96e
rk <it@indiba.in>
2016-05-10 10:48:31 +0530

    pull code

--- app/controllers/salary_slip_ledgers_controller.rb
+++ app/controllers/salary_slip_ledgers_controller.rb
@@ -1,4 +1,6 @@
+require 'query_report/helper'  # need to require the helper
 class SalarySlipLedgersController < ApplicationController
+  include QueryReport::Helper  # need to include it
   def show_employee
     @pdf = "category"
     @bank = Bank.find(params[:bank_id])
@@ -83,5 +85,32 @@ class SalarySlipLedgersController < ApplicationController
               margin:  {top:1,bottom:1,left:1,right:1 }
       end
     end
+  end
+
+  def salary_report
+    @month, @year = "January", 2016
+    @slips = []
+    @employees = Employee.first(10)
+    @employees.try(:each) do |e|
+      j = JoiningDetail.find_by_employee_id(e.id)
+      wd1 = Workingday.where('employee_id = ? and month_name = ? and year = ?', e.id, @month, @year.to_s).take
+      sl1 = Salaryslip.where('employee_id = ? and month = ? and year = ?', e.id, @month, @year.to_s).take
+      if j.nil? or e.nil? or wd1.nil? or sl1.nil?
+      else
+        sr = SalaryReport.collect_data(e,j,wd1,sl1)
+        @slips << sr
+      end
+    end
+    @sum = SalaryReport.create_sum(@slips)
+    
+    reporter(@slips, template_class: PdfReportTemplate) do
+      filter :employee_name, type: :string
+      # filter(:current_status, :enum, :select => [["Pending",0], ["FirstApproved",2], ["SecondApproved",3], ["FirstRejected",4],["SecondRejected",5],["Cancelled",1]])
+      column :employee_name
+      column :department_name
+      
+      #column(:company_location, sortable: true) { |employee_leav_request| employee_leav_request.employee.try(:company_location).try(:name) }
+    end
+
   end
 end

1c699a141f6082e7dc64772805e6db6456b02aef
rk <it@indiba.in>
2016-05-11 12:38:15 +0530

    pull code

--- app/controllers/salary_slip_ledgers_controller.rb
+++ app/controllers/salary_slip_ledgers_controller.rb
@@ -48,14 +48,14 @@ class SalarySlipLedgersController < ApplicationController
 
   def cost_unit_wise_salary
     @pdf = "cost_center"
-    @bank = Bank.find(params[:bank_id])
+    @bank = params[:bank_id]
     @category = params[:cost_center]
     @month, @year = params[:month], params[:year]
     cost_center_array = JoiningDetail.where(cost_center_id: params[:cost_center]).pluck(:employee_id)
-    emp_array = EmployeeBankDetail.where(bank_id: @bank.id).pluck(:employee_id)
+    emp_array = EmployeeBankDetail.where(bank_id: @bank).pluck(:employee_id)
     emp_user_array = Employee.collect_rolewise(current_user)
-    if cost_center_array.empty?
-      final_emp_array = emp_array & emp_user_array
+    if emp_array.empty?
+      final_emp_array = emp_user_array & cost_center_array
     else
       final_emp_array = emp_array & emp_user_array & cost_center_array
     end
@@ -88,26 +88,27 @@ class SalarySlipLedgersController < ApplicationController
   end
 
   def salary_report
-    @month, @year = "January", 2016
-    @slips = []
-    @employees = Employee.first(10)
-    @employees.try(:each) do |e|
-      j = JoiningDetail.find_by_employee_id(e.id)
-      wd1 = Workingday.where('employee_id = ? and month_name = ? and year = ?', e.id, @month, @year.to_s).take
-      sl1 = Salaryslip.where('employee_id = ? and month = ? and year = ?', e.id, @month, @year.to_s).take
-      if j.nil? or e.nil? or wd1.nil? or sl1.nil?
-      else
-        sr = SalaryReport.collect_data(e,j,wd1,sl1)
-        @slips << sr
-      end
-    end
-    @sum = SalaryReport.create_sum(@slips)
-    
+    # @month, @year = "January", 2016
+    # @slips = []
+    # @employees = Employee.first(10)
+    # @employees.try(:each) do |e|
+    #   j = JoiningDetail.find_by_employee_id(e.id)
+    #   wd1 = Workingday.where('employee_id = ? and month_name = ? and year = ?', e.id, @month, @year.to_s).take
+    #   sl1 = Salaryslip.where('employee_id = ? and month = ? and year = ?', e.id, @month, @year.to_s).take
+    #   if j.nil? or e.nil? or wd1.nil? or sl1.nil?
+    #   else
+    #     sr = SalaryReport.collect_data(e,j,wd1,sl1)
+    #     @slips << sr
+    #   end
+    # end
+    # @sum = SalaryReport.create_sum(@slips)
+    @slips = Salaryslip.all
     reporter(@slips, template_class: PdfReportTemplate) do
-      filter :employee_name, type: :string
+      filter :month, type: :string
+      filter :year, type: :string
       # filter(:current_status, :enum, :select => [["Pending",0], ["FirstApproved",2], ["SecondApproved",3], ["FirstRejected",4],["SecondRejected",5],["Cancelled",1]])
-      column :employee_name
-      column :department_name
+      column :employee_id
+      column :month
       #column(:company_location, sortable: true) { |employee_leav_request| employee_leav_request.employee.try(:company_location).try(:name) }
     end
   end

7d29a96561cecb7fc105454af775e27ac354fae4
rk <it@indiba.in>
2016-05-12 11:26:34 +0530

    push code

--- app/controllers/salary_slip_ledgers_controller.rb
+++ app/controllers/salary_slip_ledgers_controller.rb
@@ -98,18 +98,19 @@ class SalarySlipLedgersController < ApplicationController
   def collect_salary
     @month, @year, @bank = params[:month], params[:year], params[:bank_id]
     employee_bank_array = EmployeeBankDetail.where(bank_id: @bank).pluck(:employee_id)
-    @slips = Salaryslip.where(month: @month, year: @year.to_s, employee_id: employee_bank_array)
+    emp_user_array = Employee.collect_rolewise(current_user)
+    final_emp_array = employee_bank_array & emp_user_array
+    @slips = Salaryslip.where(month: @month, year: @year.to_s, employee_id: final_emp_array)
     respond_to do |f|
       f.js
       f.xls {render template: 'salary_slip_ledgers/collect_salary.xls.erb'}
       f.html
       f.pdf do
         render pdf: 'net_salary',
-              layout: 'pdf.html',
-              #orientation: 'Landscape',
-              template: 'salary_slip_ledgers/collect_salary.pdf.erb',
-              show_as_html: params[:debug].present?,
-              margin:  {top:1,bottom:1,left:1,right:1 }
+        layout: 'pdf.html',
+        template: 'salary_slip_ledgers/collect_salary.pdf.erb',
+        show_as_html: params[:debug].present?,
+        margin:  { top:1,bottom:1,left:1,right:1 }
       end
     end
   end

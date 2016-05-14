78b9c3fec92bda2d56c29cac79730ea04028b6af
indiba <it@indiba.in>
2016-05-14 10:44:40 +0530

    scroll bar, nomination

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512121825) do
+ActiveRecord::Schema.define(version: 20160514043418) do
 
   create_table "accident_records", force: :cascade do |t|
     t.string   "code"
@@ -637,20 +637,22 @@ ActiveRecord::Schema.define(version: 20160512121825) do
     t.string   "city"
     t.integer  "district_id"
     t.integer  "state_id"
-    t.integer  "country_id"
+    t.integer  "contry_id"
     t.integer  "pin_code"
     t.integer  "phone_no"
     t.integer  "mobile_no"
     t.string   "email"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
+    t.integer  "relation_master_id"
   end
 
-  add_index "employee_nominations", ["country_id"], name: "index_employee_nominations_on_country_id"
+  add_index "employee_nominations", ["contry_id"], name: "index_employee_nominations_on_contry_id"
   add_index "employee_nominations", ["district_id"], name: "index_employee_nominations_on_district_id"
   add_index "employee_nominations", ["family_id"], name: "index_employee_nominations_on_family_id"
   add_index "employee_nominations", ["nomination_master_id"], name: "index_employee_nominations_on_nomination_master_id"
   add_index "employee_nominations", ["relation_id"], name: "index_employee_nominations_on_relation_id"
+  add_index "employee_nominations", ["relation_master_id"], name: "index_employee_nominations_on_relation_master_id"
   add_index "employee_nominations", ["state_id"], name: "index_employee_nominations_on_state_id"
 
   create_table "employee_physicals", force: :cascade do |t|

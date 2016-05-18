3ce2fd12ff489a9a3273618f7de026f057ad74d5
rk <it@indiba.in>
2016-05-18 15:25:53 +0530

    push code

--- app/models/bonus_employee.rb
+++ app/models/bonus_employee.rb
@@ -30,4 +30,17 @@ class BonusEmployee < ActiveRecord::Base
     end
     amount
   end
+
+  def self.calculate_year_bonus(basic_amount)
+    amount = 0
+    bonus_master = BonusMaster.find_by_status(true)
+    unless bonus_master.nil?
+      if basic_amount <= bonus_master.limit_amount * 12
+        amount = (basic_amount / 100 * bonus_master.bonus_persentage).round
+      else
+        amount = (bonus_master.limit_amount / 100 * bonus_master.bonus_persentage).round
+      end
+    end
+    amount
+  end
 end

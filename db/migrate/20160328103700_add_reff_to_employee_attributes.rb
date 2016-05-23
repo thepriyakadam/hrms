ce55a7ea5eecab22c26c03ecc076db5422c30e3e
indiba <it@indiba.in>
2016-05-23 11:59:22 +0530

    delete migrations

--- db/migrate/20160328103700_add_reff_to_employee_attributes.rb
+++
@@ -1,5 +0,0 @@
-class AddReffToEmployeeAttributes < ActiveRecord::Migration
-  def change
-    add_reference :employee_attributes, :employee, index: true, foreign_key: true
-  end
-end
--- db/migrate/20160420133805_add_emp_ref_to_vacancy_masters.rb
+++
@@ -1,5 +0,0 @@
-class AddEmpRefToVacancyMasters < ActiveRecord::Migration
-  def change
-    add_reference :vacancy_masters, :employee, index: true, foreign_key: true
-  end
-end

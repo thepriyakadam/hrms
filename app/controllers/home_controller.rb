95232f3ae6332377b649ff45454b25973f3a63ac
vh <it@indiba.in>
2016-05-28 11:51:54 +0530

    pull

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     @companies = Company.all
     @company_locations = CompanyLocation.all
     @departments = Department.all
-    @employees = Employee.all
+    #@employees = Employee.all
     if current_user.class == Member
       if current_user.role.name == 'Employee'
         @employee = Employee.find(current_user.employee_id)

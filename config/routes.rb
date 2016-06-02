40a6828783a3fc5843f4d70678de2b11c7c04213
indiba <it@indiba.in>
2016-06-02 16:24:00 +0530

    pdf

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     collection do 
     get :all_induction_detail_list
     get :confirm
+    post :print_induction_details
     end
   end 
   resources :induction_activities do

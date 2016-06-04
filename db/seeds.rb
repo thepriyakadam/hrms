22daf2c633f070789c0d0b7f7410c77a76e321ff
vh <it@indiba.in>
2016-06-04 17:15:20 +0530

    pull

--- db/seeds.rb
+++ db/seeds.rb
@@ -250,40 +250,40 @@
 
 ###############################################################################################
 
-# ex = Roo::Excel.new("#{Rails.root}/public/workingday.xls")
-# ex.default_sheet = ex.sheets[17]
-# i = 1
-# gross_salary = 0
-# ActiveRecord::Base.transaction do
-# #2.upto(372) do |line|
-# 2.upto(91) do |line|
-#   puts "Starting Record #{ex.cell(line,'A')}---------------------------------------"
-#   @employee = Employee.find_by_manual_employee_code(ex.cell(line,'A').to_i)
-#   unless @employee.nil?
-#     Workingday.new do |w|
-#       w.employee_id = @employee.id
-#       w.month_name = ex.cell(line,'B')
-#       w.year = ex.cell(line,'C').to_i
+ex = Roo::Excel.new("#{Rails.root}/public/workingday.xls")
+ex.default_sheet = ex.sheets[19]
+i = 1
+gross_salary = 0
+ActiveRecord::Base.transaction do
+#2.upto(372) do |line|
+2.upto(372) do |line|
+  puts "Starting Record #{ex.cell(line,'A')}---------------------------------------"
+  @employee = Employee.find_by_manual_employee_code(ex.cell(line,'A').to_i)
+  unless @employee.nil?
+    Workingday.new do |w|
+      w.employee_id = @employee.id
+      w.month_name = ex.cell(line,'B')
+      w.year = ex.cell(line,'C').to_i
 
-#       w.lwp_leave = ex.cell(line, 'D').to_i
-#       w.cl_leave = ex.cell(line, 'E').to_i
-#       w.el_leave = ex.cell(line, 'F').to_i
-#       w.esic_leave = ex.cell(line, 'G').to_i
+      w.lwp_leave = ex.cell(line, 'D').to_i
+      w.cl_leave = ex.cell(line, 'E').to_i
+      w.el_leave = ex.cell(line, 'F').to_i
+      w.esic_leave = ex.cell(line, 'G').to_i
 
-#       w.day_in_month = ex.cell(line, 'H')
-#       w.present_day = ex.cell(line, 'I')
-#       w.holiday_in_month = ex.cell(line, 'J')
+      w.day_in_month = ex.cell(line, 'H')
+      w.present_day = ex.cell(line, 'I')
+      w.holiday_in_month = ex.cell(line, 'J')
 
-#       w.week_off_day = ex.cell(line, 'K')
-#       w.absent_day = ex.cell(line, 'L')
-#       w.payable_day = ex.cell(line, 'M')
-#       w.save!
-#     end
-#     puts "#{i} Record inserted.-----------------------------------------------"
-#     i = i+1
-#   end
-# end
-# end
+      w.week_off_day = ex.cell(line, 'K')
+      w.absent_day = ex.cell(line, 'L')
+      w.payable_day = ex.cell(line, 'M')
+      w.save!
+    end
+    puts "#{i} Record inserted.-----------------------------------------------"
+    i = i+1
+  end
+end
+end
 #####################################################################
 # ex = Roo::Excel.new("#{Rails.root}/public/Workingdaya.xls")
 # #ex.default_sheet = ex.sheets[6] #dewas jan

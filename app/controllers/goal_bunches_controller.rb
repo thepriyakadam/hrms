dc00b827a9c5c659bed20012fac4e468afc21c65
indiba <it@indiba.in>
2016-06-07 12:36:52 +0500

    changes

--- app/controllers/goal_bunches_controller.rb
+++ app/controllers/goal_bunches_controller.rb
@@ -142,16 +142,12 @@ class GoalBunchesController < ApplicationController
     redirect_to appraisee_comment_goal_bunches_path
   end
 
-<<<<<<< HEAD
-
   def appraiser_subordinate
     current_login = Employee.find(current_user.employee_id)
     @employees = current_login.subordinates
     session[:active_tab] ="performancemgmt"
   end
 
-=======
->>>>>>> e33d27f098bb287df36b727cb3995d4586387db2
   def appraisee_comment
     @employee = Employee.find(current_user.employee_id)
     @goal_bunch_id = GoalBunch.find(params[:id]) 

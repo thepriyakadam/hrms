0ced6d1202f4eebe0f5ee4fa3e1cc9f5fc4a5b42
indiba <it@indiba.in>
2016-06-07 12:27:11 +0500

    changes

--- app/controllers/goal_bunches_controller.rb
+++ app/controllers/goal_bunches_controller.rb
@@ -15,7 +15,7 @@ class GoalBunchesController < ApplicationController
   # GET /goal_bunches/new
   def new
     @goal_bunch = GoalBunch.new
-    @goal_bunches = GoalBunch.all
+    @goal_bunches = GoalBunch.where(employee_id: current_user.employee_id)
     session[:active_tab] ="performancemgmt"
    
   end
@@ -374,7 +374,7 @@ class GoalBunchesController < ApplicationController
     @experiences = Experience.where(employee_id: @employee.id)
     @ctc = EmployeeSalaryTemplate.where(employee_id: @employee.id).sum(:monthly_amount)
     @goal_ratings = GoalRating.where(appraisee_id: @employee.id, goal_bunch_id: @goal_bunch.id)
-    @goal_bunches = GoalBunch.where(employee_id: @employee.id).group(:employee_id)
+    @goal_bunches = GoalBunch.where(employee_id: @employee.id, id: @goal_bunch.id).group(:employee_id)
   end
 
   def print_final_detail
@@ -515,15 +515,16 @@ class GoalBunchesController < ApplicationController
   end
 
   def xl_sheet_print
-    # byebug
-    @employee = Employee.find(params[:id])
+    @employee = Employee.find(params[:emp_id])
+    @goal_bunch = GoalBunch.find(params[:id])
+
     @employees = Employee.where(id: @employee.id)
     @qualifications = Qualification.where(employee_id: @employee.id)
     @joining_detail = JoiningDetail.find_by_employee_id(@employee.id)
     @experiences = Experience.where(employee_id: @employee.id)
     @ctc = EmployeeSalaryTemplate.where(employee_id: @employee.id).sum(:monthly_amount)
     @goal_ratings = GoalRating.where(appraisee_id: @employee.id)
-    @goal_bunches = GoalBunch.where(employee_id: @employee.id)                                 
+    @goal_bunches = GoalBunch.where(employee_id: @employee.id,id: @goal_bunch.id)                                 
   end
 
   def goal_period_list

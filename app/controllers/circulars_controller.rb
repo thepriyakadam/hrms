2de01286c6d3d75ab4a9c9285adc80d1b2f70f4a
indiba <it@indiba.in>
2016-06-16 14:18:13 +0530

    work on circular form

---
+++ app/controllers/circulars_controller.rb
@@ -0,0 +1,74 @@
+class CircularsController < ApplicationController
+  before_action :set_circular, only: [:show, :edit, :update, :destroy]
+
+  # GET /circulars
+  # GET /circulars.json
+  def index
+    @circulars = Circular.all
+  end
+
+  # GET /circulars/1
+  # GET /circulars/1.json
+  def show
+  end
+
+  # GET /circulars/new
+  def new
+    @circular = Circular.new
+    @circulars = Circular.all
+  end
+
+  # GET /circulars/1/edit
+  def edit
+  end
+
+  # POST /circulars
+  # POST /circulars.json
+  def create
+     @circular = Circular.new(circular_params)
+     @circulars = Circular.all
+      if @circular.save
+        @circular = Circular.new
+      end
+      redirect_to new_circular_path
+      flash[:notice] = 'Circular was saved Successfully'   
+  end
+
+
+
+  # PATCH/PUT /circulars/1
+  # PATCH/PUT /circulars/1.json
+ 
+  def update
+    @circular.update(circular_params)
+    @circulars = Circular.all
+    @circular = Circular.new
+  end
+
+  # DELETE /circulars/1
+  # DELETE /circulars/1.json
+
+  def destroy
+    @circular.destroy
+    @circulars = Circular.all
+  end
+
+  def download_documents
+    @circular = Circular.find(params[:id])
+    send_file @circular.avatar.path,
+              filename: @circular.avatar_file_name,
+              type: @circular.avatar_content_type,
+              disposition: 'attachment'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_circular
+      @circular = Circular.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def circular_params
+      params.require(:circular).permit(:avatar, :date, :subject)
+    end
+end

0f3be20457aa451c44e1157f4d95ec914667d31d
vh <it@indiba.in>
2016-06-18 10:55:54 +0530

    pull

--- db/schema.rb
+++ db/schema.rb
@@ -832,7 +832,7 @@ ActiveRecord::Schema.define(version: 20160617092902) do
     t.text     "task_name"
     t.date     "task_date"
     t.boolean  "status"
-    t.string   "time"
+    t.time     "task_time"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
@@ -2242,6 +2242,7 @@ ActiveRecord::Schema.define(version: 20160617092902) do
     t.integer  "degree_id"
     t.string   "experience"
     t.string   "keyword"
+    t.string   "others"
     t.string   "other_organization"
     t.string   "industry"
     t.integer  "degree_1_id"

80de9288ad918d31f4ad2b47aa2c259851ed4ffb
indiba <it@indiba.in>
2016-06-22 17:51:17 +0530

    pms

--- db/schema.rb
+++ db/schema.rb
@@ -171,6 +171,15 @@ ActiveRecord::Schema.define(version: 20160621055818) do
   add_index "attribute_rating_sheets", ["final_id_id"], name: "index_attribute_rating_sheets_on_final_id_id"
   add_index "attribute_rating_sheets", ["final_rating_id"], name: "index_attribute_rating_sheets_on_final_rating_id"
 
+  create_table "attributes", force: :cascade do |t|
+    t.string   "code"
+    t.string   "name"
+    t.text     "definition"
+    t.boolean  "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "awards", force: :cascade do |t|
     t.integer  "employee_id"
     t.string   "award_name"
@@ -645,8 +654,12 @@ ActiveRecord::Schema.define(version: 20160621055818) do
     t.string   "allign_to_supervisor"
     t.boolean  "is_confirm"
     t.string   "emp_head"
+    t.integer  "appraiser_id"
+    t.integer  "appraiser2_id"
   end
 
+  add_index "employee_goals", ["appraiser2_id"], name: "index_employee_goals_on_appraiser2_id"
+  add_index "employee_goals", ["appraiser_id"], name: "index_employee_goals_on_appraiser_id"
   add_index "employee_goals", ["employee_id"], name: "index_employee_goals_on_employee_id"
   add_index "employee_goals", ["goal_perspective_id"], name: "index_employee_goals_on_goal_perspective_id"
   add_index "employee_goals", ["period_id"], name: "index_employee_goals_on_period_id"
@@ -1692,6 +1705,28 @@ ActiveRecord::Schema.define(version: 20160621055818) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "performance_activities", force: :cascade do |t|
+    t.string   "code"
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "performance_calendars", force: :cascade do |t|
+    t.integer  "period_id"
+    t.integer  "performance_activity_id"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "performance_period_id"
+  end
+
+  add_index "performance_calendars", ["performance_activity_id"], name: "index_performance_calendars_on_performance_activity_id"
+  add_index "performance_calendars", ["performance_period_id"], name: "index_performance_calendars_on_performance_period_id"
+  add_index "performance_calendars", ["period_id"], name: "index_performance_calendars_on_period_id"
 
   create_table "performance_periods", force: :cascade do |t|
     t.string   "title"

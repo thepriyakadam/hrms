30ed2897e0e89106b813c487bce32c8f7e0b4019
indiba <it@indiba.in>
2016-06-23 19:22:11 +0530

    new pull

--- app/controllers/training_plans_controller.rb
+++ app/controllers/training_plans_controller.rb
@@ -30,9 +30,13 @@ class TrainingPlansController < ApplicationController
   # POST /training_plans.json
   def create
     @training_plan = TrainingPlan.new(training_plan_params)
-
+    # byebug
     respond_to do |format|
       if @training_plan.save
+        @employee_ids = params[:employee_ids]
+        @employee_ids.each do |eid|
+        Trainee.create(employee_id: eid,training_plan_id: @training_plan.id)
+      end
         format.html { redirect_to @training_plan, notice: 'Training plan was successfully created.' }
         format.json { render :show, status: :created, location: @training_plan }
       else
@@ -41,6 +45,7 @@ class TrainingPlansController < ApplicationController
       end
     end
   end
+
 
   # PATCH/PUT /training_plans/1
   # PATCH/PUT /training_plans/1.json

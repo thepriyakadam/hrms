c0713d0e8cc747045f9a1d44e769e770b0fd23a0
indiba <it@indiba.in>
2016-06-29 11:04:01 +0530

    goals attribute

--- db/schema.rb
+++ db/schema.rb
@@ -171,6 +171,15 @@ ActiveRecord::Schema.define(version: 20160627110446) do
   add_index "attribute_rating_sheets", ["final_id_id"], name: "index_attribute_rating_sheets_on_final_id_id"
   add_index "attribute_rating_sheets", ["final_rating_id"], name: "index_attribute_rating_sheets_on_final_rating_id"
 
+  create_table "attributes", force: :cascade do |t|
+    t.string   "code"
+    t.string   "name"
+    t.text     "definition"
+    t.boolean  "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "awards", force: :cascade do |t|
     t.integer  "employee_id"
     t.string   "award_name"
@@ -660,8 +669,12 @@ ActiveRecord::Schema.define(version: 20160627110446) do
     t.string   "allign_to_supervisor"
     t.boolean  "is_confirm"
     t.string   "emp_head"
+    t.integer  "appraiser_id"
+    t.integer  "appraiser2_id"
   end
 
+  add_index "employee_goals", ["appraiser2_id"], name: "index_employee_goals_on_appraiser2_id"
+  add_index "employee_goals", ["appraiser_id"], name: "index_employee_goals_on_appraiser_id"
   add_index "employee_goals", ["employee_id"], name: "index_employee_goals_on_employee_id"
   add_index "employee_goals", ["goal_perspective_id"], name: "index_employee_goals_on_goal_perspective_id"
   add_index "employee_goals", ["period_id"], name: "index_employee_goals_on_period_id"

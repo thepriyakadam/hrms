da95a4297544fc7bd62c0e2edeb958ccff9f9b83
rk <it@indiba.in>
2016-07-01 11:56:00 +0530

    push code

--- app/models/slip_information.rb
+++ app/models/slip_information.rb
@@ -9,19 +9,27 @@ class SlipInformation < ActiveRecord::Base
   		s.salaryslip_id = salaryslip.id
   		s.cost_center_id = @joining_detail.cost_center_id
   		s.department_id = employee.department_id
-  		#s.contact_no = employ
+  		s.contact_no = employee.contact_no
   		s.esic_no = @joining_detail.employee_efic_no
   		s.pf_no = @joining_detail.employee_pf_no
   		s.uan_no = @joining_detail.employee_uan_no
-  		# s.cl = 
-  		# s.el = 
-  		# s.c_off = 
+  		# s.cl = workingday.cl_leave
+  		# s.el = workingday.el_leave
+  		# s.c_off = workingday.coff_leave
   		# s.advance = 
-  		# s.cl_b = 
-  		# s.el_b = 
-  		# s.c_off_b = 
-  		# s.advance_b =
-  		s.save 
+      @leave_balances = EmployeeLeavBalance.where(employee_id: employee.id)
+      @leave_balances.each do |b|
+        if b.leav_category.name == "Casual Leave"
+          s.cl_b = b.no_of_leave
+        elsif b.leav_category.name == "Earned Leave"
+          s.el_b = b.no_of_leave
+        elsif b.leav_category.name == "Advance Leave"
+          s.advance_b = b.no_of_leave
+        elsif b.leav_category.name == "C.Off"
+          s.c_off_b = b.no_of_leave
+        end
+        s.save
+      end
   	end
   end
 end

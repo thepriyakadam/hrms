9c117081211bc5365653480e0cd3d8e1e07ef761
indiba <it@indiba.in>
2016-07-02 17:20:57 +0530

    employee attendance show page

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
       post :department_wise_employee_list
       post :all_emp_list
       post :create_employee_attendance
+      get :attendance
     end
   end
   resources :salary_comp_mappings

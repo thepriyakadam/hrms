88b4fbd7c9d361305384ac331816d2b5e542d097
rk <it@indiba.in>
2016-07-06 01:59:28 +0530

    push code

--- app/controllers/workingdays_controller.rb
+++ app/controllers/workingdays_controller.rb
@@ -99,10 +99,12 @@ class WorkingdaysController < ApplicationController
           end
         end
       end
-      workingday.present_day = Attendance.where(attendance_date: @date.beginning_of_month..@date.end_of_month, employee_id: e.id).count
+      #workingday.present_day = Attendance.where(attendance_date: @date.beginning_of_month..@date.end_of_month, employee_id: e.id).count
       workingday.total_leave = ParticularLeaveRecord.where(leave_date: @date.beginning_of_month..@date.end_of_month, employee_id: e.id).count
       workingday.holiday_in_month = Holiday.where(holiday_date: @date.beginning_of_month..@date.end_of_month).count
       workingday.week_off_day = WeekoffMaster.day(@date)
+      workingday.absent_day = workingday.holiday_in_month.to_i + workingday.week_off_day.to_i + workingday.total_leave.to_f
+      workingday.present_day = workingday.day_in_month.to_i - workingday.absent_day
       workingday.employee_id = e.id
       @workingdays << workingday
     end

cdfbd3e8288ee70cee8d808979e38a65ba3647e2
BFTLHRMS <it@indiba.in>
2016-07-09 12:14:06 +0530

    pull

--- db/schema.rb
+++ db/schema.rb
@@ -918,7 +918,7 @@ ActiveRecord::Schema.define(version: 20160708113856) do
     t.text     "task_name"
     t.date     "task_date"
     t.boolean  "status"
-    t.string   "time"
+    t.time     "task_time"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
@@ -2150,15 +2150,6 @@ ActiveRecord::Schema.define(version: 20160708113856) do
   add_index "selected_resumes", ["degree_id"], name: "index_selected_resumes_on_degree_id"
   add_index "selected_resumes", ["vacancy_master_id"], name: "index_selected_resumes_on_vacancy_master_id"
 
-  create_table "setting_masters", force: :cascade do |t|
-    t.string   "date"
-    t.string   "precision"
-    t.string   "timeformat"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "shift_rotations", force: :cascade do |t|
     t.integer  "company_shift_id"
     t.date     "start_date"
@@ -2437,6 +2428,7 @@ ActiveRecord::Schema.define(version: 20160708113856) do
     t.integer  "degree_id"
     t.string   "experience"
     t.string   "keyword"
+    t.string   "others"
     t.string   "other_organization"
     t.string   "industry"
     t.integer  "degree_1_id"

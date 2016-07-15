15259993cb355f9229c59e52ace27005391b5510
indiba <it@indiba.in>
2016-07-15 18:56:10 +0530

    recognition_type

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,4 +1,4 @@
 class Period < ActiveRecord::Base
 	has_many :goal_bunches
-	 validates :name, presence: true,uniqueness: { case_sensitive: false }
+	validates :name, presence: true,uniqueness: { case_sensitive: false }
 end

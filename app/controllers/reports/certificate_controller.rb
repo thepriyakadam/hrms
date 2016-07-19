c631c97c3b4e9ba471c38380cc73189ca1ce680c
indiba <it@indiba.in>
2016-07-19 19:20:38 +0530

    certificate

--- app/controllers/reports/certificate_controller.rb
+++ app/controllers/reports/certificate_controller.rb
@@ -4,8 +4,10 @@ class Reports::CertificateController < ApplicationController
   def new
   end
 
-  def display_certificate
-  	@employee = Employee.find(params[:employee][:id])
-  	@character = params[:employee][:certificate]
+  def show_joining_date
+  	@emp = params[:salary][:employee]
+  	@employee = Employee.find(@emp)
+  	@joining_detail = JoiningDetail.find_by_employee_id(@employee.id)
+  	#@character = params[:employee][:certificate]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -443,8 +443,8 @@ end
     
     post 'salaries/ctc_yearly_report'
     get 'salaries/ctc_yearly'
-     get 'certificate/new'
-     get 'certificate/display_certificate'
+    get 'certificate/new'
+    post 'certificate/show_joining_date'
   end
 
   namespace :views do

38054e0919f7fc81b343201ad8980eb582ab240c
rk <it@indiba.in>
2016-07-28 15:24:46 +0530

    stage code

--- app/controllers/certificates_controller.rb
+++ app/controllers/certificates_controller.rb
@@ -1,20 +1,20 @@
 class CertificatesController < ApplicationController
-
   def show
   end
   
   def new
   end
 
-def show_joining_date
+  def show_joining_date
+    @employee = Employee.find(params[:salary][:employee])
+    @certificate = params[:salary][:certificate]
+    @joining_detail = JoiningDetail.find_by_employee_id(@employee.id)
+  end
+
+  def certificate_print
     @employee = Employee.find(params[:salary][:employee])
     @certificate = params[:salary][:certificate]
     @joining_detail = JoiningDetail.find_by_employee_id(@employee.id)
   end
 end
 
-def certificate_print
-    @employee = Employee.find(params[:salary][:employee])
-    @certificate = params[:salary][:certificate]
-    @joining_detail = JoiningDetail.find_by_employee_id(@employee.id)  
-end

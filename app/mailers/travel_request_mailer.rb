22d3aa6cd4338f60d5a12a0efd08c0040bb7e230
indiba <it@indiba.in>
2016-08-16 19:06:55 +0530

    travel check

--- app/mailers/travel_request_mailer.rb
+++ app/mailers/travel_request_mailer.rb
@@ -7,27 +7,25 @@ class TravelRequestMailer < ApplicationMailer
      @travel_request = TravelRequest.find(travel_request.id)
      @emp = TravelRequest.find_by_employee_id(travel_request.employee_id)
      mail(to: @employee.email, subject: 'Travel Request')
-
     end
 
     def approve_travel_request_email(travel_request)
     @travel_request = travel_request
-    @reporting_master = Employee.find(travel_request.reporting_master_id)
+    @reporting_master = Employee.find(travel_request.reporting_master.employee_id)
     @travel_request = TravelRequest.find(travel_request.id)
     @employee = Employee.find(@travel_request.employee_id)
     @emp = TravelRequest.find_by_employee_id(travel_request.employee_id)
-    @rep1 = Employee.find(travel_request.reporting_master_id)
+    @rep1 = Employee.find(travel_request.reporting_master.employee_id)
     mail(to: @employee.email, subject: 'Travel Request Approved')
     end
 
- 
     def reject_travel_request_email(travel_request)
     @travel_request = travel_request
     @reporting_master = Employee.find(travel_request.reporting_master.employee_id)
     @travel_request = TravelRequest.find(travel_request.id)
     @employee = Employee.find(@travel_request.employee_id)
     @emp = TravelRequest.find_by_employee_id(travel_request.employee_id)
-    @rep1 = Employee.find(travel_request.reporting_master_id)
+    @rep1 = Employee.find(travel_request.reporting_master.employee_id)
     mail(to: @employee.email, subject: 'Travel Request Rejected')
     end
 

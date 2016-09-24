e4c2583f283db82da0e953b35f92329f3601f8ba
rk <it@indiba.in>
2016-09-24 15:39:53 +0530

    stage code

--- app/controllers/employee_salary_templates_controller.rb
+++ app/controllers/employee_salary_templates_controller.rb
@@ -84,17 +84,17 @@ class EmployeeSalaryTemplatesController < ApplicationController
     else
       arrears_array = params[:old_salary_component_id].keys
       increement_date = params[:increement][:date]
-      @employee_template = EmployeeTemplate.create_new_object(@employee_id, @template_id)
-      @employee_template = EmployeeTemplate.build_objects(arrays, params, @employee_id, @template_id, @employee_template)
+      #@employee_template = EmployeeTemplate.create_new_object(@employee_id, @template_id)
+      #@employee_template = EmployeeTemplate.build_objects(arrays, params, @employee_id, @template_id, @employee_template)
 
-      @employee_arrear = EmployeeArrear.create_object(@employee_id, increement_date)
-      @employee_arrear = EmployeeArrear.build_objects(arrears_array, params, @employee_arrear)
-      if @employee_arrear.save
+      #@employee_arrear = EmployeeArrear.create_object(@employee_id, increement_date)
+      #@employee_arrear = EmployeeArrear.build_objects(arrears_array, params, @employee_arrear)
+      #if @employee_arrear.save
         @employee_template.save
         flash[:notice] = 'Employee template created successfully.'
-      else
-        flash[:alert] = 'Same template cannot assigned.'
-      end
+      #else
+        #flash[:alert] = 'Same template cannot assigned.'
+      #end
       redirect_to template_list_employee_templates_path(@employee_id)
     end
   end
@@ -145,7 +145,7 @@ class EmployeeSalaryTemplatesController < ApplicationController
         arrear_start_year = arrear_start_date.strftime('%Y').to_i
         arrear_end_month = Workingday.months[@month]
         arrear_end_year = params['year'].to_i
-        #byebug
+
         arrear_working_days = Workingday.where(employee_id: @employee.id, month_name: arrear_start_month..arrear_end_month, year: arrear_start_year..arrear_end_year)
         @total_payable_days = arrear_working_days.sum('payable_day')
         @arrear_items = @arrear.employee_arrear_items

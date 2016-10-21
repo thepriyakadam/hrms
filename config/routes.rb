2658b263a67387ee7e704ba0858101858239c8a0
vh <it@indiba.in>
2016-10-21 10:54:45 +0530

    pull

--- config/routes.rb
+++ config/routes.rb
@@ -641,6 +641,7 @@ end
     get 'monthly_expences_details/new'
 
     get 'society_membership_details/new'
+    post 'society_membership_details/society_membership_report'
 
     get 'overtime_salary_details/daily'
     post 'overtime_salary_details/overtime_daily_detail_report'
@@ -662,6 +663,7 @@ end
     get 'retention_money_details/new'
 
     get 'welfare_details/new'
+    post 'welfare_details/welfare_detail_report'
 
     get 'esic_details/new'
     post 'esic_details/esic_detail_report'

77fb1ae0f0579ee27733c5e88e887e5d3452005f
indiba <it@indiba.in>
2016-10-22 17:58:48 +0530

    salaryslip

--- db/schema.rb
+++ db/schema.rb
@@ -2506,15 +2506,6 @@ ActiveRecord::Schema.define(version: 20161019103720) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "reward_allocations", force: :cascade do |t|
-    t.string   "code"
-    t.string   "name"
-    t.text     "description"
-    t.boolean  "status"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
   create_table "reward_owners", force: :cascade do |t|
     t.string   "code"
     t.string   "name"

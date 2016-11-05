87086b8609ee483bb135c83fdf218a9768dad0ca
indiba <it@indiba.in>
2016-11-05 11:42:29 +0530

    new

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104093342) do
+ActiveRecord::Schema.define(version: 20161105060406) do
 
   create_table "about_bosses", force: :cascade do |t|
     t.string   "code"
@@ -2705,6 +2705,8 @@ ActiveRecord::Schema.define(version: 20161104093342) do
     t.date     "validity_date"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.boolean  "is_active"
+    t.boolean  "is_confirm"
   end
 
   create_table "salaryslip_components", force: :cascade do |t|

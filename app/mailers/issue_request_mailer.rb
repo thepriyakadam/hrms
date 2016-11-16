c2cb0de53008376974912501f74326093df84010
indiba <it@indiba.in>
2016-11-16 14:05:07 +0530

    new

--- app/mailers/issue_request_mailer.rb
+++ app/mailers/issue_request_mailer.rb
@@ -15,11 +15,12 @@ class IssueRequestMailer < ApplicationMailer
   end
 
 
-def issue_tracker_member(issue_request)
-      @employee = Employee.find(:id)
-      @issue_tracker_member = IssueTrackerMember.where
-      @issue_request = IssueRequest.find(issue_request.id)
-      @emp = EmployeeResignation.find_by_employee_id(issue_request.employee_id)
-      mail(to: @employee.email, subject: 'Resignation Request')
-  end
+# def issue_tracker_member_email(issue_request)
+#   @issue_request = issue_request
+#   @issue_tracker_member = IssueTrackerMember.find(@issue_request.issue_tracker_group_id)
+#   @employee = Employee.find(@issue_request.employee_id)
+#   @issue_tracker_group = IssueTrackerGroup.find(@issue_request.employee_id)
+#   mail(to: @issue_tracker_member.email, subject: 'Support Request')
+# end
+      
 end

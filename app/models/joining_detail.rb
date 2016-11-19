6e09bcd44a54601d30b1a27e8962ce102e4bc14e
indiba <it@indiba.in>
2016-11-19 11:51:27 +0530

    new

--- app/models/joining_detail.rb
+++ app/models/joining_detail.rb
@@ -17,6 +17,35 @@ class JoiningDetail < ActiveRecord::Base
   # validate :notice_period_regex
   # validate :status_regex
 
+
+def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |joining_detail|
+        csv << joining_detail.attributes.values_at(*column_names)
+      end
+    end
+  end
+
+  def self.import(file)
+    spreadsheet = open_spreadsheet(file)
+    header = spreadsheet.row(1)
+    (2..spreadsheet.last_row).each do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+      joining_detail = find_by_id(row["id"]) || new
+      joining_detail.attributes = row.to_hash.slice(*row.to_hash.keys)
+      joining_detail.save!
+    end
+  end
+
+  def self.open_spreadsheet(file)
+    case File.extname(file.original_filename)
+      when ".csv" then Roo::CSV.new(file.path, file_warning: :ignore)
+      when ".xls" then Roo::Excel.new(file.path, file_warning: :ignore)
+      when ".xlsx" then Roo::Excelx.new(file.path, file_warning: :ignore)
+      else raise "Unknown file type: #{file.original_filename}"
+    end
+  end
   def probation_period_regex
     if probation_period.present? && !probation_period.match(/\A[0-9A-Za-z_ ]{1,30}\Z/)
       errors.add :probation_period, 'Please Enter Probation Period In Days or Months'
--- config/routes.rb
+++ config/routes.rb
@@ -1506,7 +1506,13 @@ end
       patch :update_role
     end
   end
-  resources :employee_bank_details
+  resources :employee_bank_details do
+    collection do
+      post :import  
+      get :import_xl
+    end
+  end
+
   resources :leav_cancelleds
 
   resources :employee_leav_balances do
@@ -1580,6 +1586,8 @@ end
   end
   resources :employees do
     collection do
+      get :import_xl
+      post :import
       get :graph
       get :assign_role
       post :submit_form
@@ -1636,6 +1644,14 @@ end
       get :transfer_form
     end
   end
+
+  # resources :joining_details do
+  #   collection do
+  #     get :import_xl
+  #     post :import
+  #   end
+  # end
+
   resources :blood_groups do
     collection do
       get :is_confirm

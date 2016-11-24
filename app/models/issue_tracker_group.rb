3cbf4b4c47c142a1c28b83abaa031ef5d874a4f8
indiba3 <it@indiba.in>
2016-11-24 17:24:53 +0530

    new pull

--- app/models/issue_tracker_group.rb
+++ app/models/issue_tracker_group.rb
@@ -4,6 +4,4 @@ class IssueTrackerGroup < ActiveRecord::Base
  has_many :issue_masters
  has_many :issue_tracker_groups
  has_many :issue_root_causes
- validates :name, presence: true
- validates :email, presence: true
 end

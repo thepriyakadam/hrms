dd1d0e219e3f8420fd1881e4b175d9b25d70ced8
vh <it@indiba.in>
2017-02-13 15:03:09 +0530

    pull

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-    default from: "hrmsdws@bhandarigroup.in"
-    # default from: "ranna.p@amaazia.com"
+    # default from: "hrmsdws@bhandarigroup.in"
+     default from: "ranna.p@amaazia.com"
   layout 'mailer'
 end

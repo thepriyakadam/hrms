12609a5aed8e7b46bf7911d6632b64b7fc9429a0
vh <it@indiba.in>
2017-02-14 15:31:38 +0530

    pull

--- db/seeds.rb
+++ db/seeds.rb
@@ -1221,6 +1221,32 @@ ex = Roo::Excel.new("#{Rails.root}/public/rgwdd.xls")
 #    end
 #  end
 
+ex = Roo::Excel.new("#{Rails.root}/public/rgfdf.xls")
+ ex.default_sheet = ex.sheets[2] #siya feb
+ i = 1
+ ActiveRecord::Base.transaction do
+
+ 2.upto(112) do |line| # siya Feb 201
+   puts "Starting Record #{ex.cell(line,'A')}---------------------------------------"
+   @employee = Employee.find_by_manual_employee_code(ex.cell(line,'A').to_i)
+   unless @employee.nil?
+
+     FoodDeduction.new do |w|
+       w.employee_id = @employee.id
+       w.no_of_coupan = ex.cell(line, 'B')
+       w.return_coupan = ex.cell(line, 'C').to_i
+       w.total_coupan = ex.cell(line, 'D')
+       w.food_coupan_master_id = ex.cell(line, 'E').to_i
+       w.amount = ex.cell(line, 'F')
+       w.food_date = ex.cell(line, 'G')
+       w.save!
+     end
+     puts "#{i} Record inserted.-----------------------------------------------"
+     i += 1
+   end
+   end
+ end
+
 
 
 # puts "Starting ..."

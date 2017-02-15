46b30688b68eca91d6ee6e06b32fc40bf0884532
vh <it@indiba.in>
2017-02-15 14:18:52 +0530

    pull

--- db/seeds.rb
+++ db/seeds.rb
@@ -1141,54 +1141,56 @@ require 'roo'
 # i = i+1
 # end
 
+SlipInformation.destroy_all
 
-ex = Roo::Excel.new("#{Rails.root}/public/rgwdd.xls")
- ex.default_sheet = ex.sheets[0] #siya feb
- i = 1
- ActiveRecord::Base.transaction do
 
- 2.upto(155) do |line| # siya Feb 201
-   puts "Starting Record #{ex.cell(line,'A')}---------------------------------------"
-   @employee = Employee.find_by_manual_employee_code(ex.cell(line,'A').to_i)
-   unless @employee.nil?
+# ex = Roo::Excel.new("#{Rails.root}/public/rgwdd.xls")
+#  ex.default_sheet = ex.sheets[0] #siya feb
+#  i = 1
+#  ActiveRecord::Base.transaction do
 
-     Workingday.new do |w|
-       w.employee_id = @employee.id
-       w.month_name = ex.cell(line, 'B')
-       w.year = ex.cell(line, 'C').to_i
-       w.day_in_month = ex.cell(line, 'D')
-       w.payable_day = ex.cell(line, 'E')
-       w.save!
-     end
-     puts "#{i} Record inserted.-----------------------------------------------"
-     i += 1
-   end
-   end
- end
+#  2.upto(155) do |line| # siya Feb 201
+#    puts "Starting Record #{ex.cell(line,'A')}---------------------------------------"
+#    @employee = Employee.find_by_manual_employee_code(ex.cell(line,'A').to_i)
+#    unless @employee.nil?
 
- ex = Roo::Excel.new("#{Rails.root}/public/rgwdj.xls")
- ex.default_sheet = ex.sheets[1] #siya feb
- i = 1
- ActiveRecord::Base.transaction do
+#      Workingday.new do |w|
+#        w.employee_id = @employee.id
+#        w.month_name = ex.cell(line, 'B')
+#        w.year = ex.cell(line, 'C').to_i
+#        w.day_in_month = ex.cell(line, 'D')
+#        w.payable_day = ex.cell(line, 'E')
+#        w.save!
+#      end
+#      puts "#{i} Record inserted.-----------------------------------------------"
+#      i += 1
+#    end
+#    end
+#  end
 
- 1.upto(150) do |line| # siya Feb 201
-   puts "Starting Record #{ex.cell(line,'A')}---------------------------------------"
-   @employee = Employee.find_by_manual_employee_code(ex.cell(line,'A').to_i)
-   unless @employee.nil?
+#  ex = Roo::Excel.new("#{Rails.root}/public/rgwdj.xls")
+#  ex.default_sheet = ex.sheets[1] #siya feb
+#  i = 1
+#  ActiveRecord::Base.transaction do
 
-     Workingday.new do |w|
-       w.employee_id = @employee.id
-       w.month_name = ex.cell(line, 'B')
-       w.year = ex.cell(line, 'C').to_i
-       w.day_in_month = ex.cell(line, 'D')
-       w.payable_day = ex.cell(line, 'E')
-       w.save!
-     end
-     puts "#{i} Record inserted.-----------------------------------------------"
-     i += 1
-   end
-   end
- end
+#  1.upto(150) do |line| # siya Feb 201
+#    puts "Starting Record #{ex.cell(line,'A')}---------------------------------------"
+#    @employee = Employee.find_by_manual_employee_code(ex.cell(line,'A').to_i)
+#    unless @employee.nil?
+
+#      Workingday.new do |w|
+#        w.employee_id = @employee.id
+#        w.month_name = ex.cell(line, 'B')
+#        w.year = ex.cell(line, 'C').to_i
+#        w.day_in_month = ex.cell(line, 'D')
+#        w.payable_day = ex.cell(line, 'E')
+#        w.save!
+#      end
+#      puts "#{i} Record inserted.-----------------------------------------------"
+#      i += 1
+#    end
+#    end
+#  end
 
 # ex = Roo::Excel.new("#{Rails.root}/public/joining_detail_report.xls")
 # ex.default_sheet = ex.sheets[2] #siya feb

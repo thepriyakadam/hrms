14d4707457dd8e6ccd8bc985332d0ef96c67ea7d
indiba <it@indiba.in>
2017-02-20 18:38:51 +0530

    new

--- app/models/employee_type.rb
+++ app/models/employee_type.rb
@@ -2,4 +2,5 @@ class EmployeeType < ActiveRecord::Base
   has_many :employees
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :code, presence: true, uniqueness: { case_sensitive: false }
 end

1076c5a4d4728bcfee3efa67170b4ecf7e48ed13
indiba <it@indiba.in>
2017-02-22 14:51:20 +0530

    pull

--- app/controllers/pdf_salaries_controller.rb
+++ app/controllers/pdf_salaries_controller.rb
@@ -22,10 +22,10 @@ class PdfSalariesController < ApplicationController
               layout: '/layouts/pdf.html.erb',
               :template => 'pdf_salaries/salary_slip_company_location_department.pdf.erb',
               # :orientation      => 'Landscape', # default , Landscape
-              :page_height      => 1200,
+              :page_height      => 1000,
               :dpi              => '300',
-              :margin           => {:top    => 30, # default 10 (mm)
-                            :bottom => 30,
+              :margin           => {:top    => 20, # default 10 (mm)
+                            :bottom => 90,
                             :left   => 10,
                             :right  => 10},
               :show_as_html => params[:debug].present?

cf56ab6ed2d682542207729a799ff99638f20858
indiba3 <it@indiba.in>
2017-03-06 18:10:16 +0530

    new pull

--- app/models/induction_activity.rb
+++ app/models/induction_activity.rb
@@ -9,8 +9,8 @@ class InductionActivity < ActiveRecord::Base
   validates :facilitator_1, presence: true
   # validates :employee_id, presence: true
 
-  has_attached_file :avatar, path: ':rails_root/attachments_induction_activity/induction_activity/:style/:basename.:extension' 
+  # has_attached_file :avatar, path: ':rails_root/attachments_induction_activity/induction_activity/:style/:basename.:extension' 
   # validates_attachment_presence :avatar
-  validates_attachment_size :avatar, less_than: 2.megabytes
-  validates_attachment :avatar, content_type: { content_type: %w(application/pdf application/msword application/msexcel application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
+  # validates_attachment_size :avatar, less_than: 2.megabytes
+  # validates_attachment :avatar, content_type: { content_type: %w(application/pdf application/msword application/msexcel application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
 end

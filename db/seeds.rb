459c56170c31e2aea67b17da5e6b9815323d8619
vh <it@indiba.in>
2017-03-09 11:13:57 +0530

    pull

--- db/seeds.rb
+++ db/seeds.rb
@@ -1284,31 +1284,31 @@ require 'roo'
 #  end
 # end
 
-ex = Roo::Excel.new("#{Rails.root}/public/rgfdj.xls")
- ex.default_sheet = ex.sheets[3] #siya feb
- i = 1
- ActiveRecord::Base.transaction do
+# ex = Roo::Excel.new("#{Rails.root}/public/rgfdj.xls")
+#  ex.default_sheet = ex.sheets[3] #siya feb
+#  i = 1
+#  ActiveRecord::Base.transaction do
 
- 2.upto(77) do |line| # siya Feb 201
-   puts "Starting Record #{ex.cell(line,'A')}---------------------------------------"
-   @employee = Employee.find_by_manual_employee_code(ex.cell(line,'A').to_i)
-   unless @employee.nil?
+#  2.upto(77) do |line| # siya Feb 201
+#    puts "Starting Record #{ex.cell(line,'A')}---------------------------------------"
+#    @employee = Employee.find_by_manual_employee_code(ex.cell(line,'A').to_i)
+#    unless @employee.nil?
 
-     FoodDeduction.new do |w|
-       w.employee_id = @employee.id
-       w.no_of_coupan = ex.cell(line, 'B')
-       w.return_coupan = ex.cell(line, 'C').to_i
-       w.total_coupan = ex.cell(line, 'D')
-       w.food_coupan_master_id = ex.cell(line, 'E').to_i
-       w.amount = ex.cell(line, 'F')
-       w.food_date = ex.cell(line, 'G')
-       w.save!
-     end
-     puts "#{i} Record inserted.-----------------------------------------------"
-     i += 1
-   end
-   end
- end
+#      FoodDeduction.new do |w|
+#        w.employee_id = @employee.id
+#        w.no_of_coupan = ex.cell(line, 'B')
+#        w.return_coupan = ex.cell(line, 'C').to_i
+#        w.total_coupan = ex.cell(line, 'D')
+#        w.food_coupan_master_id = ex.cell(line, 'E').to_i
+#        w.amount = ex.cell(line, 'F')
+#        w.food_date = ex.cell(line, 'G')
+#        w.save!
+#      end
+#      puts "#{i} Record inserted.-----------------------------------------------"
+#      i += 1
+#    end
+#    end
+#  end
 
 # ex = Roo::Excel.new("#{Rails.root}/public/rgfdf.xls")
 #  ex.default_sheet = ex.sheets[2] #siya feb

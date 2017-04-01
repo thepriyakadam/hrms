d91fa8125860ac8f0728afecc38e7a3bc18984ce
indiba <it@indiba.in>
2017-04-01 10:00:46 +0530

    pull

--- app/controllers/employee_leav_balances_controller.rb
+++ app/controllers/employee_leav_balances_controller.rb
@@ -85,14 +85,14 @@ class EmployeeLeavBalancesController < ApplicationController
                   if @leave_master.is_carry_forward == true
                     @leave = @leave_master.no_of_leave.to_f + e.no_of_leave.to_f
                     if @leave <= @leave_master.limit.to_f
-                      EmployeeLeavBalance.create(employee_id: e.employee_id,leav_category_id: e.leav_category_id,no_of_leave: @leave,from_date: e.to_date,to_date: date_quarterly,expiry_date: date_quarterly,is_active: true,total_leave: e.no_of_leave)
+                      EmployeeLeavBalance.create(employee_id: e.employee_id,leav_category_id: e.leav_category_id,no_of_leave: @leave,from_date: e.to_date,to_date: date_quarterly,expiry_date: date_quarterly,is_active: true,total_leave: @leave)
                       e.update(is_active: false)
                     else
-                      EmployeeLeavBalance.create(employee_id: e.employee_id,leav_category_id: e.leav_category_id,no_of_leave: @leave_master.limit,from_date: e.to_date,to_date: date_quarterly,expiry_date: date_quarterly,is_active: true,total_leave: e.no_of_leave)
+                      EmployeeLeavBalance.create(employee_id: e.employee_id,leav_category_id: e.leav_category_id,no_of_leave: @leave_master.limit,from_date: e.to_date,to_date: date_quarterly,expiry_date: date_quarterly,is_active: true,total_leave: @leave_master.limit)
                       e.update(is_active: false)
                     end #limit
                   else
-                    EmployeeLeavBalance.create(employee_id: e.employee_id,leav_category_id: e.leav_category_id,no_of_leave: @leave_master.no_of_leave,from_date: e.to_date,to_date: date_quarterly,expiry_date: date_quarterly,is_active: true,total_leave: e.no_of_leave)
+                    EmployeeLeavBalance.create(employee_id: e.employee_id,leav_category_id: e.leav_category_id,no_of_leave: @leave_master.no_of_leave,from_date: e.to_date,to_date: date_quarterly,expiry_date: date_quarterly,is_active: true,total_leave: @leave_master.no_of_leave)
                     e.update(is_active: false)
                   end #is_carry_forward
                 else #working_day = nil
@@ -107,10 +107,10 @@ class EmployeeLeavBalancesController < ApplicationController
                     # @employee_actual_workingday
                       if e.total_leave.to_f < @leave_master.working_day.to_f
                         if @leave_master.is_carry_forward == true
-                          EmployeeLeavBalance.create(employee_id: e.employee_id,leav_category_id: e.leav_category_id,no_of_leave: e.no_of_leave.to_f,from_date: e.to_date,to_date: date_quarterly,expiry_date: date_quarterly,is_active: true,total_leave: @employee_actual_workingday)
+                          EmployeeLeavBalance.create(employee_id: e.employee_id,leav_category_id: e.leav_category_id,no_of_leave: e.no_of_leave.to_f,from_date: e.to_date,to_date: date_quarterly,expiry_date: date_quarterly,is_active: true,total_leave: e.no_of_leave.to_f)
                           e.update(is_active: false)
                         else 
-                          EmployeeLeavBalance.create(employee_id: e.employee_id,leav_category_id: e.leav_category_id,no_of_leave: 0,from_date: e.to_date,to_date: date_quarterly,expiry_date: date_quarterly,is_active: true,total_leave: @employee_actual_workingday)
+                          EmployeeLeavBalance.create(employee_id: e.employee_id,leav_category_id: e.leav_category_id,no_of_leave: 0,from_date: e.to_date,to_date: date_quarterly,expiry_date: date_quarterly,is_active: true,total_leave: 0)
                           e.update(is_active: false)
                         end #is_carry_forward
                       else #@employee_actual_workingday < workingday
@@ -120,16 +120,16 @@ class EmployeeLeavBalancesController < ApplicationController
                         if @leave_master.is_carry_forward == true
                           @leave = @calculated_no_of_leave.to_f + e.no_of_leave.to_f
                           if @leave <= @leave_master.limit.to_f
-                            EmployeeLeavBalance.create(employee_id: e.employee_id,leav_category_id: e.leav_category_id,no_of_leave: @leave.round,from_date: e.to_date,to_date: date_quarterly,is_active: true,expiry_date: date_quarterly,total_leave: @employee_actual_workingday)
+                            EmployeeLeavBalance.create(employee_id: e.employee_id,leav_category_id: e.leav_category_id,no_of_leave: @leave.round,from_date: e.to_date,to_date: date_quarterly,is_active: true,expiry_date: date_quarterly,total_leave: @leave.round)
                             e.update(is_active: false)
                             flash[:notice] = "Created Successfully"
                           else
-                            EmployeeLeavBalance.create(employee_id: e.employee_id,leav_category_id: e.leav_category_id,no_of_leave: @leave_master.limit.to_f,from_date: e.to_date,to_date: date_quarterly,is_active: true,expiry_date: date_quarterly,total_leave: @employee_actual_workingday)
+                            EmployeeLeavBalance.create(employee_id: e.employee_id,leav_category_id: e.leav_category_id,no_of_leave: @leave_master.limit.to_f,from_date: e.to_date,to_date: date_quarterly,is_active: true,expiry_date: date_quarterly,total_leave: @leave_master.limit.to_f)
                             e.update(is_active: false)
                             flash[:notice] = "Created Successfully"
                           end #@leave <= limit
                         else #is_carry_forward
-                          EmployeeLeavBalance.create(employee_id: e.employee_id,leav_category_id: e.leav_category_id,no_of_leave: calculated_no_of_leave.to_f,from_date: e.to_date,to_date: date_quarterly,is_active: true,expiry_date: date_quarterly,total_leave:  @employee_actual_workingday)
+                          EmployeeLeavBalance.create(employee_id: e.employee_id,leav_category_id: e.leav_category_id,no_of_leave: calculated_no_of_leave.to_f,from_date: e.to_date,to_date: date_quarterly,is_active: true,expiry_date: date_quarterly,total_leave:  calculated_no_of_leave.to_f)
                           e.update(is_active: false)
                         end
                       end # @day

428548920d24c667540b0b2d42e32aeeea959e40
indiba <it@indiba.in>
2017-04-07 11:47:06 +0530

    new

--- app/models/visitor_detail.rb
+++ app/models/visitor_detail.rb
@@ -1,4 +1,4 @@
 class VisitorDetail < ActiveRecord::Base
-  belongs_to :meet_to
-  belongs_to :authorized_by
+  belongs_to :meet_to, class_name: 'Employee'
+  belongs_to :authorized_by, class_name: 'Employee'
 end

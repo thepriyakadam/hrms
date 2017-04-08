5e275ef8e9fbd6e890326f4d9592e0b314b24f4f
indiba <it@indiba.in>
2017-04-08 05:29:11 -0400

    new pull

--- db/migrate/20170404101934_create_slabs.rb
+++ db/migrate/20170404101934_create_slabs.rb
@@ -1,13 +1,13 @@
 class CreateSlabs < ActiveRecord::Migration
   def change
-    create_table :slabs do |t|
-      t.decimal :from
-      t.decimal :to
-      t.decimal :texable_amount
-      t.decimal :percentage
-      t.boolean :status
+    # create_table :slabs do |t|
+    #   t.decimal :from
+    #   t.decimal :to
+    #   t.decimal :texable_amount
+    #   t.decimal :percentage
+    #   t.boolean :status
 
-      t.timestamps null: false
-    end
+    #   t.timestamps null: false
+    # end
   end
 end
--- db/migrate/20170405090450_create_texable_amounts.rb
+++ db/migrate/20170405090450_create_texable_amounts.rb
@@ -1,13 +1,13 @@
 class CreateTexableAmounts < ActiveRecord::Migration
   def change
-    create_table :texable_amounts do |t|
-      t.references :employee, index: true, foreign_key: true
-      t.decimal :yearly
-      t.decimal :monthly
-      t.decimal :remaining_amount
-      t.boolean :is_paid
+    # create_table :texable_amounts do |t|
+    #   t.references :employee, index: true, foreign_key: true
+    #   t.decimal :yearly
+    #   t.decimal :monthly
+    #   t.decimal :remaining_amount
+    #   t.boolean :is_paid
 
-      t.timestamps null: false
-    end
+    #   t.timestamps null: false
+    # end
   end
 end

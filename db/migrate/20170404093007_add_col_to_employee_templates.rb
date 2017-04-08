576f83246bf2a3ae90493ce0f599fa6db14d1177
indiba <it@indiba.in>
2017-04-08 03:38:35 -0400

    new pull

--- db/migrate/20170404093007_add_col_to_employee_templates.rb
+++ db/migrate/20170404093007_add_col_to_employee_templates.rb
@@ -1,5 +1,5 @@
 class AddColToEmployeeTemplates < ActiveRecord::Migration
   def change
-    add_column :employee_templates, :gross_salary, :decimal
+    # add_column :employee_templates, :gross_salary, :decimal
   end
 end

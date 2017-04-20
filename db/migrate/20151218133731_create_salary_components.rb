c42192a907b0f00ee8464fcc611c54a8b6d8330e
vh <it@indiba.in>
2017-04-20 12:04:36 +0530

    pull

--- db/migrate/20151218133731_create_salary_components.rb
+++ db/migrate/20151218133731_create_salary_components.rb
@@ -4,7 +4,7 @@ class CreateSalaryComponents < ActiveRecord::Migration
       t.string :code
       t.string :name
       t.string :description
-      t.reference :parent, index: true, foreign_key: true
+      t.references :parent, index: true, foreign_key: true
       t.boolean :is_deducted
       t.boolean :is_confirm
       t.timestamps null: false

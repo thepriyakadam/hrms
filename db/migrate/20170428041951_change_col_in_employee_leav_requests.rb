df57d17b754db91ae22dd9b55555bb76d0ddec99
cpu019 <it@indiba.in>
2017-04-28 17:53:33 +0530

    push

--- db/migrate/20170428041951_change_col_in_employee_leav_requests.rb
+++ db/migrate/20170428041951_change_col_in_employee_leav_requests.rb
@@ -1,7 +1,7 @@
 class ChangeColInEmployeeLeavRequests < ActiveRecord::Migration
   def change
-  	# remove_reference :employee_leav_requests, :first_reporter, index: true, foreign_key: true
-  	# remove_reference :employee_leav_requests, :second_reporter, index: true, foreign_key: true
+  	remove_reference :employee_leav_requests, :first_reporter, index: true, foreign_key: true
+  	remove_reference :employee_leav_requests, :second_reporter, index: true, foreign_key: true
 
   	add_reference :employee_leav_requests ,:first_reporter
   	add_reference  :employee_leav_requests ,:second_reporter

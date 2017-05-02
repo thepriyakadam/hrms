2fc7e423be89f4b223d9c3eacbe966aad4d33839
vh <it@indiba.in>
2017-05-02 09:53:56 +0530

    pull

--- db/migrate/20170420105215_change_datatype_from_workingdays.rb
+++ db/migrate/20170420105215_change_datatype_from_workingdays.rb
@@ -1,28 +1,28 @@
 class ChangeDatatypeFromWorkingdays < ActiveRecord::Migration
   def change
-  	change_column :workingdays, :present_day, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :total_leave, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :holiday_in_month, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :week_off_day, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :absent_day, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :payable_day, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :lwp_leave, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :cl_leave, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :el_leave, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :esic_leave, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :coff_leave, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :advance_leave, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :cl_balance, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :el_balance, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :coff_balance, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :advance_balance, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :pay_leave, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :nonpay_leave, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :gatepass, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :calculated_payable_days, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :ot_hours, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :od_leave, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :od_day, :decimal, precision: 5, scale: 1
-  	change_column :workingdays, :nonpayable_day, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :present_day, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :total_leave, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :holiday_in_month, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :week_off_day, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :absent_day, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :payable_day, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :lwp_leave, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :cl_leave, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :el_leave, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :esic_leave, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :coff_leave, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :advance_leave, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :cl_balance, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :el_balance, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :coff_balance, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :advance_balance, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :pay_leave, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :nonpay_leave, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :gatepass, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :calculated_payable_days, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :ot_hours, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :od_leave, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :od_day, :decimal, precision: 5, scale: 1
+  	# change_column :workingdays, :nonpayable_day, :decimal, precision: 5, scale: 1
   end
 end

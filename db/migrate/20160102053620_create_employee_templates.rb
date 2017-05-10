1dcc9a8956c1f7e96c8b344d29f59d943f27669a
hradmin <hradmin>
2017-05-09 23:43:32 -0700

    pull

---
+++ db/migrate/20160102053620_create_employee_templates.rb
@@ -0,0 +1,13 @@
+class CreateEmployeeTemplates < ActiveRecord::Migration
+  def change
+    create_table :employee_templates do |t|
+      t.references :employee, index: true, foreign_key: true
+      t.references :salary_template, index: true, foreign_key: true
+      t.boolean :is_active, default: true
+      t.date :start_date
+      t.date :end_date
+
+      t.timestamps null: false
+    end
+  end
+end

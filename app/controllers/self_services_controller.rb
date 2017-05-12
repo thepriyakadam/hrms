7e63994912a07cf7b051396bdfa839fde30a0a97
cpu019 <it@indiba.in>
2017-05-12 17:13:32 +0530

    pull

--- app/controllers/self_services_controller.rb
+++ app/controllers/self_services_controller.rb
@@ -1,9 +1,6 @@
 class SelfServicesController < ApplicationController
+  #before_action :set_self_service, only: [:show, :edit, :update, :destroy]
 
-
- def show
- end
- 
   def employee
     @employees = Employee.where(id: current_user.employee_id)
     session[:active_tab] ="EmployeeSelfService"
@@ -73,29 +70,23 @@ class SelfServicesController < ApplicationController
   end
 
   def show_self_datewise_attendance
-    # byebug
     @from = params[:employee][:from]
     @to = params[:employee][:to]
     @employee_attendances = EmployeeAttendance.where(day: @from.to_date..@to.to_date,employee_id: current_user.employee_id)
     
-    respond_to do |format|
-      format.js
-      format.xls {render template: 'self_services/datewise_attendance_report_xls.xls.erb'}
-      format.html
-      format.pdf do
-        render pdf: 'datewise_attendance_report_pdf',
-              layout: 'pdf.html',
-              orientation: 'Landscape',
-              template: 'self_services/datewise_attendance_report_pdf.pdf.erb',
-              # show_as_html: params[:debug].present?,
-              :page_height      => 1000,
-              :dpi              => '300',
-              :margin           => {:top    => 10, # default 10 (mm)
-                            :bottom => 10,
-                            :left   => 20,
-                            :right  => 20},
-              :show_as_html => params[:debug].present?
-          end
+
+      respond_to do |f|
+      f.js
+      f.xls {render template: 'self_services/datewise_attendance_report_xls.xls.erb'}
+      f.html
+      f.pdf do
+        render pdf: 'self_service',
+        layout: 'pdf.html',
+        orientation: 'Landscape',
+        template: 'self_services/datewise_attendance_report_pdf.pdf.erb',
+        show_as_html: params[:debug].present?
+      end
     end
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511092455) do
+ActiveRecord::Schema.define(version: 20170509105811) do
 
   create_table "about_bosses", force: :cascade do |t|
     t.string   "code",        limit: 255

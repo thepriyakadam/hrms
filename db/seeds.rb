28cf9c08c5e46753294aa8a2bc8a40de076bc18c
vh <it@indiba.in>
2017-05-12 16:04:39 +0530

    pull

--- db/seeds.rb
+++ db/seeds.rb
@@ -372,62 +372,108 @@ require 'roo'
 
 #================================ DESIGNATION =====================================#
 
+#================================ EMPLOYEE BASIC DETAIL START =========================
 
+# puts "Starting ..."
+# ex = Roo::Excel.new("#{Rails.root}/public/e.xls")
+# ex.default_sheet = ex.sheets[0] 
+# i=1
 
-puts "Starting ..."
-ex = Roo::Excel.new("#{Rails.root}/public/sg_employee_detail.xls")
-ex.default_sheet = ex.sheets[0] 
-i=1
-
-2.upto(424) do |line|
-Employee.new do |e|
-  e.manual_employee_code = ex.cell(line,'A').to_i
-  e.first_name = ex.cell(line,'B')
-  e.middle_name = ex.cell(line,'C')
-  e.last_name = ex.cell(line,'D')
-  e.gender = ex.cell(line,'E')
-  e.adhar_no = ex.cell(line,'F').to_i
-  e.pan_no = ex.cell(line,'G')
-  e.licence_no = ex.cell(line,'H')
-  e.marital_status = ex.cell(line,'I')
-  @nation = Nationality.find_by_name(ex.cell(line,'J'))
-  e.nationality_id = @nation.id unless @nation.nil?
-  e.date_of_birth = ex.cell(line,'K')
-  @blood = BloodGroup.find_by_name(ex.cell(line,'L'))
-  e.blood_group_id = @blood.id unless @blood.nil?
-  e.permanent_address = ex.cell(line,'M')
-  e.pin_code = ex.cell(line,'N').to_i
-  @country = Country.find_by_name(ex.cell(line,'O'))
-  e.country_id = @country.id unless @country.nil?
-  @state = State.find_by_name(ex.cell(line,'P'))
-  e.state_id = @state.id unless @state.nil?
-  @district = District.find_by_name(ex.cell(line,'Q'))
-  e.district_id =  @district.id unless @district.nil?
-  e.city = ex.cell(line,'R')
-  e.current_address = ex.cell(line,'S')
-  @religion = Religion.find_by_name(ex.cell(line,'T'))
-  e.religion_id = @religion.id unless @religion.nil?
-  e.contact_no = ex.cell(line,'U').to_i
-  e.email = ex.cell(line,'V')
-  e.handicap = ex.cell(line,'W')
-  e.handicap_type = ex.cell(line, 'X')
+# 2.upto(424) do |line|
+# Employee.new do |e|
+#   e.manual_employee_code = ex.cell(line,'A').to_i
+#   e.first_name = ex.cell(line,'B')
+#   e.middle_name = ex.cell(line,'C')
+#   e.last_name = ex.cell(line,'D')
+#   e.gender = ex.cell(line,'E')
+#   e.adhar_no = ex.cell(line,'F').to_i
+#   e.pan_no = ex.cell(line,'G')
+#   e.licence_no = ex.cell(line,'H')
+#   e.marital_status = ex.cell(line,'I')
+#   @nation = Nationality.find_by_name(ex.cell(line,'J'))
+#   e.nationality_id = @nation.id unless @nation.nil?
+#   e.date_of_birth = ex.cell(line,'K')
+#   @blood = BloodGroup.find_by_name(ex.cell(line,'L'))
+#   e.blood_group_id = @blood.id unless @blood.nil?
+#   e.permanent_address = ex.cell(line,'M')
+#   e.pin_code = ex.cell(line,'N').to_i
+#   @country = Country.find_by_name(ex.cell(line,'O'))
+#   e.country_id = @country.id unless @country.nil?
+#   @state = State.find_by_name(ex.cell(line,'P'))
+#   e.state_id = @state.id unless @state.nil?
+#   @district = District.find_by_name(ex.cell(line,'Q'))
+#   e.district_id =  @district.id unless @district.nil?
+#   e.city = ex.cell(line,'R')
+#   e.current_address = ex.cell(line,'S')
+#   @religion = Religion.find_by_name(ex.cell(line,'T'))
+#   e.religion_id = @religion.id unless @religion.nil?
+#   e.contact_no = ex.cell(line,'U').to_i
+#   e.email = ex.cell(line,'V')
+#   e.handicap = ex.cell(line,'W')
+#   e.handicap_type = ex.cell(line, 'X')
   
-  @type = EmployeeType.find_by_name(ex.cell(line,'Y'))
-  e.employee_type_id = @type.id unless @type.nil?
+#   @type = EmployeeType.find_by_name(ex.cell(line,'Y'))
+#   e.employee_type_id = @type.id unless @type.nil?
 
-  e.status = ex.cell(line,'Z')
+#   e.status = ex.cell(line,'Z')
   
-  e.company_id = ex.cell(line,'AA').to_i
-  @type1 = CompanyLocation.find_by_name(ex.cell(line,'AB'))
-  e.company_location_id = @type1.id unless @type1.nil?
-  @type2 = Department.find_by_name(ex.cell(line,'AC'))
-  e.department_id =  @type2.id unless @type2.nil?
+#   e.company_id = ex.cell(line,'AA').to_i
+#   @type1 = CompanyLocation.find_by_name(ex.cell(line,'AB'))
+#   e.company_location_id = @type1.id unless @type1.nil?
+#   @type2 = Department.find_by_name(ex.cell(line,'AC'))
+#   e.department_id =  @type2.id unless @type2.nil?
 
-  @code_master = EmployeeCodeMaster.find_by_name(ex.cell(line,'AD'))
-  e.employee_code_master_id = @code_master.id unless @code_master.nil?
+#   @code_master = EmployeeCodeMaster.find_by_name(ex.cell(line,'AD'))
+#   e.employee_code_master_id = @code_master.id unless @code_master.nil?
 
-  e.save!
-end
-puts "#{i} Employee inserted.-----------------------------------------------"
-i = i+1
+#   e.save!
+# end
+# puts "#{i} Employee inserted.-----------------------------------------------"
+# i = i+1
+# end
+
+
+#========================= EMPLOYEE BASIC DATA END ===========================================
+
+
+ex = Roo::Excel.new("#{Rails.root}/public/j.xls")
+ex.default_sheet = ex.sheets[0] #siya feb
+i = 1
+ActiveRecord::Base.transaction do
+2.upto(424) do |line| # siya Feb 2016
+ puts "Starting Record --------------------------#{ex.cell(line,'A')}"
+ @employee = Employee.find_by_manual_employee_code(ex.cell(line,'A').to_i)
+ # JoiningDetail.where(id: @employee.id).update_all(is_da: true)
+ unless @employee.nil?
+
+ @joining_details = JoiningDetail.where(employee_id: @employee.id)
+  @joining_details.new do |e|
+     e.employee_uan_no = ex.cell(line,'B').to_i
+     e.joining_date =  ex.cell(line,'C')
+     e.confirmation_date =  ex.cell(line,'D')
+     @designation = EmployeeDesignation.find_by_name(ex.cell(line,'E'))
+     e.employee_designation_id = @designation.id unless @designation.nil?
+      @grade = EmployeeGrade.find_by_name(ex.cell(line,'F'))
+     e.employee_grade_id = @grade.id unless @grade.nil?
+     # e.employee_pf_no = ex.cell(line,'F').to_i
+     @category = EmployeeCategory.find_by_name(ex.cell(line,'G'))
+     e.employee_category_id = @category.id unless @category.nil?
+
+     e.probation_period = ex.cell(line,'H').to_i
+     e.probation_period = ex.cell(line,'I').to_i
+     e.notice_period =     ex.cell(line,'J').to_i
+
+     e.have_passport = ex.cell(line,'K')
+     e.passport_no = ex.cell(line,'L').to_i
+     e.passport_issue_date = ex.cell(line,'M')
+     e.passport_expiry_date = ex.cell(line,'N')
+     e.retirement_date = ex.cell(line,'O')
+    e.save!
+
+    puts "Save...."
+   end
+   puts "#{i} Record inserted.------#{ex.cell(line,'B')}--------#{ex.cell(line,'C')}---------#{ex.cell(line,'D')}.....#{ex.cell(line,'E')}......#{ex.cell(line,'G')}"
+   i += 1
+ end
+ end
 end

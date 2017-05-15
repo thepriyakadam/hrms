38ff87b459086ed763364172e3788460b19e126b
cpu019 <it@indiba.in>
2017-05-15 11:43:51 +0530

    push

---
+++ db/migrate/20170508073310_add_col_to_vacancy_master.rb
@@ -0,0 +1,6 @@
+class AddColToVacancyMaster < ActiveRecord::Migration
+  def change
+    add_column :vacancy_masters, :experience, :string
+    add_column :vacancy_masters, :keyword, :string
+  end
+end

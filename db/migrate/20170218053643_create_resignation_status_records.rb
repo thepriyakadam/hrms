229b5bfbdefbc7394cf4e22329c63bfc902fa385
vh <it@indiba.in>
2017-05-26 22:52:00 +0530

    new

--- db/migrate/20170218053643_create_resignation_status_records.rb
+++ db/migrate/20170218053643_create_resignation_status_records.rb
@@ -2,7 +2,7 @@ class CreateResignationStatusRecords < ActiveRecord::Migration
   def change
     create_table :resignation_status_records do |t|
       t.references :employee_resignation, index: true, foreign_key: true
-      t.references :change_status_employee, index: true, foreign_key: true
+      t.references :change_status_employee
       t.string :status
       t.datetime :change_date
 

77c17d82beb3403b6a92ed2d7d3fd819444b4887
Ubuntu <ubuntu@ip-172-31-15-53.ap-southeast-1.compute.internal>
2017-06-03 11:19:30 +0000

    new

--- db/migrate/20170529095137_add_columns_to_workingdays.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToWorkingdays < ActiveRecord::Migration
-  def change
-  	add_column :workingdays , :nonpayable_day , :decimal
-  	add_column :workingdays , :gatepass , :decimal
-  end
-end

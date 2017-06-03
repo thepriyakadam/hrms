15d2ce67618541ddd5b29ef50d266e31c4aeb049
cpu019 <it@indiba.in>
2017-06-03 11:32:26 +0530

    push

--- db/migrate/20170529095137_add_columns_to_workingdays.rb
+++ db/migrate/20170529095137_add_columns_to_workingdays.rb
@@ -1,6 +1,6 @@
 class AddColumnsToWorkingdays < ActiveRecord::Migration
   def change
   	add_column :workingdays , :nonpayable_day , :decimal
-  	add_column :workingdays , :gatepass , :decimal
+  	
   end
 end

31daaf3f133a56e547e8fa600cfc5083ed0a3e67
cpu019 <it@indiba.in>
2017-06-03 15:31:04 +0530

    push

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   resources :reimbursement_requests do
     collection do
       get :send_for_approval
-      get :list_for_approval
-      get :approve_request
-      get :reject_request
+      get :approval_list
+      get :approve
+      get :reject
       get :edit_request_modal
       post :update_request_modal
     end

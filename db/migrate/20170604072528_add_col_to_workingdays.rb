de327bc64e6c45363d211122d2747da6533a5879
vh <it@indiba.in>
2017-06-04 13:06:59 +0530

    pull

--- db/migrate/20170604072528_add_col_to_workingdays.rb
+++ db/migrate/20170604072528_add_col_to_workingdays.rb
@@ -1,6 +1,5 @@
 class AddColToWorkingdays < ActiveRecord::Migration
   def change
     add_column :workingdays, :gatepass, :decimal
-    add_column :workingdays, :nonpayble_day, :decimal
   end
 end

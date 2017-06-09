8da757cb9389f05c4b884d4eed93a942a4a7ef3c
dev009 <yo89ku@gmail.com>
2017-06-09 13:20:41 +0530

    new

--- db/migrate/20170603085329_remove_col_from_employee_resignation.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColFromEmployeeResignation < ActiveRecord::Migration
-  def change
-    remove_reference :employee_resignations, :change_status_employee
-     add_reference :employee_resignations, :change_status_employee
-  end
-end

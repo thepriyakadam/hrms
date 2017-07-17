ebaed8bb1dbefdca61ce4cfbdeced9122b3b98c5
cpu019 <it@indiba.in>
2017-07-17 13:38:59 +0530

    pull

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -144,22 +144,22 @@
    # }
 
 
-  config.action_mailer.default_url_options = { host: 'http://amaazia.co.in', port: 2095 }
+  # config.action_mailer.default_url_options = { host: 'http://amaazia.co.in', port: 2095 }
 
-  # config.domain = 'lvh.me'
-  # config.action_mailer.perform_deliveries = true,
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-  address: 'amaazia.co.in',
-  port:  587,
-  domain: 'amaazia.co.in',
-  user_name: 'hrms@rajgreen.co.in',
-  password: 'amaazia@123',
-  authentication: :plain,
-  enable_starttls_auto: true,
-   openssl_verify_mode: OpenSSL::SSL::VERIFY_NONE
-   # openssl_verify_mode: 'none'
-  }
+  # # config.domain = 'lvh.me'
+  # # config.action_mailer.perform_deliveries = true,
+  # ActionMailer::Base.delivery_method = :smtp
+  # ActionMailer::Base.smtp_settings = {
+  # address: 'amaazia.co.in',
+  # port:  587,
+  # domain: 'amaazia.co.in',
+  # user_name: 'hrms@rajgreen.co.in',
+  # password: 'amaazia@123',
+  # authentication: :plain,
+  # enable_starttls_auto: true,
+  #  openssl_verify_mode: OpenSSL::SSL::VERIFY_NONE
+  #  # openssl_verify_mode: 'none'
+  # }
 
  
   # # config.domain = 'lvh.me'

8b9ac2713468027e925b60bd72b51633de561f45
dev009 <yo89ku@gmail.com>
2017-07-24 10:56:26 +0530

    new

--- db/migrate/20170719110756_change_column_from_employee_resignation.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnFromEmployeeResignation < ActiveRecord::Migration
-  def change
-  	remove_reference :employee_resignations, :change_status_employee, index: true, foreign_key: true
-  	add_reference :employee_resignations, :change_status_employee
-  end
-end

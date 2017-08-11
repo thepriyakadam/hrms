1a48aec255019a186762f120ab96375fda463ade
anand chourey <it@indiba.in>
2017-08-11 15:19:33 +0530

    pull

--- db/migrate/20170809123204_add_col_to_reporting_masters_travel_requests.rb
+++
@@ -1,6 +0,0 @@
-class AddColToReportingMastersTravelRequests < ActiveRecord::Migration
-  def change
-    remove_reference :reporting_masters_travel_requests, :reporting_master, index: true, foreign_key: true
-    add_reference :reporting_masters_travel_requests, :reporting_master
-  end
-end

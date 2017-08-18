c8548ae0def5f702457b1afc6538859f8574f3d3
vh <it@indiba.in>
2017-08-18 10:16:47 +0530

    pull

---
+++ db/migrate/20160616133220_create_resignation_histories.rb
@@ -0,0 +1,29 @@
+class CreateResignationHistories < ActiveRecord::Migration
+  def change
+    create_table :resignation_histories do |t|
+      t.references :employee_resignation, index: true, foreign_key: true
+      t.references :reporting_master, index: true, foreign_key: true
+      t.date :resignation_date
+      t.string :reason
+      t.boolean :is_notice_period
+      t.string :notice_period
+      t.string :short_notice_period
+      t.date :tentative_leaving_date
+      t.text :remark
+      t.date :exit_interview_date
+      t.text :note
+      t.date :leaving_date
+      t.date :settled_on
+      t.boolean :has_left
+      t.boolean :notice_served
+      t.boolean :rehired
+      t.references :leaving_reason, index: true, foreign_key: true
+      t.references :employee, index: true, foreign_key: true
+      
+
+      t.string :resign_status
+
+      t.timestamps null: false
+    end
+  end
+end

a5998089de0274e686e4e7479dfe0d9a00a66f6a
vh <it@indiba.in>
2017-09-11 11:39:02 +0530

    pull

--- db/migrate/20170906090955_add_ref_to_training_requests.rb
+++
@@ -1,6 +0,0 @@
-class AddRefToTrainingRequests < ActiveRecord::Migration
-  def change
-  	remove_reference :training_requests, :reporting_masters, index: true, foreign_key: true
-    add_reference :training_requests, :reporting_masters
-  end
-end

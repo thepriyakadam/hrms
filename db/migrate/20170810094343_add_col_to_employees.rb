a750ba23103b48d29925c74d7848a43a7eea2f69
hradmin <hradmin>
2017-09-13 13:56:17 +0530

    new

--- db/migrate/20170810094343_add_col_to_employees.rb
+++ db/migrate/20170810094343_add_col_to_employees.rb
@@ -1,5 +1,6 @@
 class AddColToEmployees < ActiveRecord::Migration
   def change
     add_column :employees, :extension_no, :string
+    add_column :employees, :emergency_contact_no, :string
   end
 end

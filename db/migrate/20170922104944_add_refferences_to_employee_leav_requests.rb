bef1649eebfbc5cc75d39f119373aba5471aa750
anand chourey <it@indiba.in>
2017-09-22 17:27:18 +0530

    pull

---
+++ db/migrate/20170922104944_add_refferences_to_employee_leav_requests.rb
@@ -0,0 +1,5 @@
+class AddRefferencesToEmployeeLeavRequests < ActiveRecord::Migration
+  def change
+    add_reference :employee_leav_requests, :employee_leav_balance, index: true, foreign_key: true
+  end
+end

23d96fece7328e1f0038737fafc0919d8b83bf11
dev009 <yo89ku@gmail.com>
2017-12-06 14:15:12 +0530

    pull

--- db/migrate/20171120051504_remove_plan_reason_master_id_id_from_employee_plans.rb
+++
@@ -1,5 +0,0 @@
-class RemovePlanReasonMasterIdIdFromEmployeePlans < ActiveRecord::Migration
-  def change
-    # remove_column :employee_plans, :plan_reason_master_id_id, :integer
-  end
-end
--- db/migrate/20171202074828_add_references_to_family.rb
+++
@@ -1,5 +0,0 @@
-class AddReferencesToFamily < ActiveRecord::Migration
-  def change
-    # add_reference :families, :relation_master,index: true, foreign_key: true
-  end
-end

322fb67067db8f20d7cc68fca7e8cf87ad3ef9d6
Ubuntu <ubuntu@ip-172-31-27-118.ap-southeast-1.compute.internal>
2017-12-18 16:26:20 +0530

    pull

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128073429) do
+ActiveRecord::Schema.define(version: 20171215123749) do
 
   create_table "about_bosses", force: :cascade do |t|
     t.string   "code",        limit: 255
@@ -697,7 +697,7 @@ ActiveRecord::Schema.define(version: 20171128073429) do
   create_table "daily_attendances", force: :cascade do |t|
     t.string   "sr_no",         limit: 255
     t.date     "date"
-    t.time     "time"
+    t.datetime "time"
     t.string   "employee_code", limit: 255
     t.string   "card_no",       limit: 255
     t.string   "employee_name", limit: 255

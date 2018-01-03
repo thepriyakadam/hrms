9295b07295299a1d48b8a87d76731076239d4a34
vishal <design@indibasolutions.com>
2018-01-03 16:20:14 +0530

    pull

--- db/migrate/20180102042535_add_col_to_daily_attendance.rb
+++ db/migrate/20180102042535_add_col_to_daily_attendance.rb
@@ -1,8 +1,5 @@
 class AddColToDailyAttendance < ActiveRecord::Migration
   def change
-remove_column :daily_attendances, :latitude, :string
-remove_column :daily_attendances, :float, :string
-remove_column :daily_attendances, :longitude, :string 
  
     add_column :daily_attendances, :latitude, :decimal
     add_column :daily_attendances, :longitude, :decimal

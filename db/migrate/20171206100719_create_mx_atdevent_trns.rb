09a5643b016b7961f24224ceba4b915fd08e594e
vinod <design@indibasolutions.com>
2018-01-05 12:51:06 +0530

    committed

--- db/migrate/20171206100719_create_mx_atdevent_trns.rb
+++ db/migrate/20171206100719_create_mx_atdevent_trns.rb
@@ -1,10 +1,10 @@
 class CreateMxAtdeventTrns < ActiveRecord::Migration
-	ActiveRecord::Base.establish_connection("development_sec")
-  	def change
-    	create_table :mx_atdevent_trns do |t|
+	# ActiveRecord::Base.establish_connection("development_sec")
+ #  	def change
+ #    	create_table :mx_atdevent_trns do |t|
 
-    	  	t.timestamps null: false
-    	end
-  	end
-  ActiveRecord::Base.establish_connection(Rails.env)
+ #    	  	t.timestamps null: false
+ #    	end
+ #  	end
+ #  ActiveRecord::Base.establish_connection(Rails.env)
 end

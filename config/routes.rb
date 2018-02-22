cd2a2d0aa26156de1e1f381bd2d415ce9077873e
Sam <it@indiba.in>
2018-02-22 15:56:16 +0530

    push

--- config/routes.rb
+++ config/routes.rb
@@ -1204,6 +1204,9 @@ end
       post :import
       get :goal_set
       get :admin_level_goal_set
+      get :admin_level_period
+      get :admin_appraiser_evaluation_period
+      get :admin_level_appraiser_evaluation
     end
   end
   #post 'goal_ratings/update_goal_set_modal'
@@ -1280,6 +1283,8 @@ end
       post :create_admin_level_period
       get :admin_goal_approval_period
       get :admin_level_goal_approval
+      get :admin_self_evaluation_period
+      get :admin_level_self_evaluation
     end
   end
   resources :goal_perspectives do

f1d998308a7c76c3259ab722fb52934e8b60134f
indiba <design@indibasolutions.com>
2018-03-05 19:54:07 +0530

    vinod jagtap

--- config.ru
+++ config.ru
@@ -3,8 +3,8 @@
 require ::File.expand_path('../config/environment', __FILE__)
 run Rails.application
 
-require "rack-timeout"
+# require "rack-timeout"
 
 # Call as early as possible so rack-timeout runs before all other middleware.
 # Setting service_timeout is recommended. If omitted, defaults to 15 seconds.
-use Rack::Timeout, service_timeout: 9000
+# use Rack::Timeout, service_timeout: 900
--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,4 +1,4 @@
-Rack::Timeout.service_timeout = 900  # seconds
+# Rack::Timeout.service_timeout = 900  # seconds
 
-# insert middleware wherever you want in the stack, optionally pass initialization arguments
-Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 200
+# # insert middleware wherever you want in the stack, optionally pass initialization arguments
+# Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 200

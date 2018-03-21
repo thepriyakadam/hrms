5cf002fe8b4e82345f4288b4fc0305d320c93db8
dev009 <yo89ku@gmail.com>
2018-03-21 19:21:20 +0530

    pull

---
+++ db/migrate/20180110055516_add_confirm_to_employee_plan.rb
@@ -0,0 +1,6 @@
+class AddConfirmToEmployeePlan < ActiveRecord::Migration
+  def change
+  	remove_column :employee_plans, :conform, :boolean
+ 	add_column :employee_plans, :confirm, :boolean
+  end
+end

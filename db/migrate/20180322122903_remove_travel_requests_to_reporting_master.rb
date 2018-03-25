eaaaf45525b51c303b8b6c130b227cde3f9f5af3
vishal <design@indibasolutions.com>
2018-03-25 23:32:14 +0530

    pull

--- db/migrate/20180322122903_remove_travel_requests_to_reporting_master.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTravelRequestsToReportingMaster < ActiveRecord::Migration
-  def change
-    remove_reference :travel_requests, :reporting_master, index: true, foreign_key: true
-  end
-end
--- db/migrate/20180322125018_add_ref_to_travel_requests.rb
+++
@@ -1,5 +0,0 @@
-class AddRefToTravelRequests < ActiveRecord::Migration
-  def change
-     add_reference :travel_requests, :reporting_master 
-  end
-end

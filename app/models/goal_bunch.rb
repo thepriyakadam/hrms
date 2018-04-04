71cd05557d63127b9488038b117b4a968eae804e
vishal <design@indibasolutions.com>
2018-04-04 15:39:44 +0530

    pul'

--- app/models/goal_bunch.rb
+++ app/models/goal_bunch.rb
@@ -1,5 +1,4 @@
-
-​class GoalBunch < ActiveRecord::Base
+class GoalBunch < ActiveRecord::Base
   belongs_to :period
   belongs_to :employee
   belongs_to :appraisee
@@ -8,15 +7,15 @@
   belongs_to :final
   has_many :goal_ratings
   belongs_to :appraisee_rating
- 
+
   belongs_to :r_designation, class_name: 'EmployeeDesignation'
   belongs_to :f_designation, class_name: 'EmployeeDesignation'
-   
+ 
   belongs_to :reviewer_rating, class_name: 'Rating'
   belongs_to :final_rating, class_name: 'Rating'
- 
+
   validates_uniqueness_of :period_id, :scope => :employee_id
- 
+
   def self.import(file)
      spreadsheet = open_spreadsheet(file)
      (2..spreadsheet.last_row).each do |i|
@@ -28,9 +27,9 @@
         end
         self_rating = spreadsheet.cell(i,'L')
         goal_rating_id = spreadsheet.cell(i,'M')
- 
+
           if goal_rating_id == nil
-          else     
+          else    
             goal_rating = GoalRating.find_by(id: goal_rating_id)
         period = Period.find_by(id: goal_rating.period_id)
             int_rating = self_rating.to_i
@@ -39,8 +38,8 @@
               @self_rating_id = 0
             else
               @self_rating = Rating.find_by(value: rating.to_f)
-              if period.marks == true  
-                 
+              if period.marks == true
+                
                 if @self_rating == nil
                   rating = Rating.last
                   rating_id = rating.id
@@ -62,7 +61,7 @@
                     weightage_rating_id = weightage_rating.id
                     @self_rating_id = weightage_rating_id.to_f
                   end
-                end#@self_rating == nil       
+                end#@self_rating == nil      
               else
                 if @self_rating == nil
                   rating = Rating.last
@@ -72,7 +71,7 @@
                 end
               end#period.marks == true
             end#rating == nil
- 
+
             goal_rating = GoalRating.find_by(id: goal_rating_id)
             if goal_rating_id.to_i == goal_rating.id
               goal_rating.update(appraisee_comment: self_comment,appraisee_rating_id: @self_rating_id)
@@ -80,7 +79,7 @@
           end# goal_rating_id == nil
     end#do
   end
- 
+
    def self.import_appraiser_evaluation(file)
      spreadsheet = open_spreadsheet(file)
      (2..spreadsheet.last_row).each do |i|
@@ -90,12 +89,12 @@
           else
             appraiser_comment = spreadsheet.cell(i,'M')
           end
- 
+
           appraiser_rating = spreadsheet.cell(i,'N')
           goal_rating_id = spreadsheet.cell(i,'O')
- 
+
           if goal_rating_id == nil
-          else  
+          else
             int_rating = appraiser_rating.to_i
             rating = int_rating.to_s
             if rating == nil
@@ -116,7 +115,7 @@
           end#goal_rating_id == nil
       end#do
     end
- 
+
   def self.import_reviewer_evaluation(file)
     spreadsheet = open_spreadsheet(file)
      (2..spreadsheet.last_row).each do |i|
@@ -128,13 +127,13 @@
         end
         goal_rating_id = spreadsheet.cell(i,'P')
         if goal_rating_id == nil
-        else  
+        else
           goal_rating = GoalRating.find_by(id: goal_rating_id)
           goal_rating.update(reviewer_comment: reviewer_comment)
         end#goal_rating_id == nil
     end#do
   end
- 
+
   def self.open_spreadsheet(file)
     case File.extname(file.original_filename)
       when ".csv" then Roo::CSV.new(file.path, file_warning: :ignore)
@@ -143,26 +142,26 @@
       else raise "Unknown file type: #{file.original_filename}"
     end
   end
- 
+
   def self.appraisergoal_sum(appraiser_goal_ratings)
     appraiser_goal_sum = 0
-    appraiser_goal_ratings.each do |goal_rating|      
-      weightage = goal_rating.goal_weightage  
-      appraiser_rating = goal_rating.appraiser_rating_id  
-      appraiser_output = weightage * appraiser_rating.to_i / 100.to_f  
-      appraiser_goal_sum = appraiser_goal_sum + appraiser_output  
+    appraiser_goal_ratings.each do |goal_rating|     
+      weightage = goal_rating.goal_weightage
+      appraiser_rating = goal_rating.appraiser_rating_id
+      appraiser_output = weightage * appraiser_rating.to_i / 100.to_f
+      appraiser_goal_sum = appraiser_goal_sum + appraiser_output
     end
     appraiser_goal_sum
   end
- 
+
   def self.appraiser_attribute_sum(appraiser_attribute_ratings)
-    appraiser_attribute_sum = 0  
-    appraiser_attribute_ratings.each do |goal_rating|      
-      weightage = goal_rating.goal_weightage  
-      appraiser_rating = goal_rating.appraiser_rating_id  
-      appraiser_output = weightage * appraiser_rating.to_i / 100.to_f  
-      appraiser_attribute_sum = appraiser_attribute_sum + appraiser_output  
+    appraiser_attribute_sum = 0
+    appraiser_attribute_ratings.each do |goal_rating|     
+      weightage = goal_rating.goal_weightage
+      appraiser_rating = goal_rating.appraiser_rating_id
+      appraiser_output = weightage * appraiser_rating.to_i / 100.to_f
+      appraiser_attribute_sum = appraiser_attribute_sum + appraiser_output
     end
     appraiser_attribute_sum
   end
-end 
+end

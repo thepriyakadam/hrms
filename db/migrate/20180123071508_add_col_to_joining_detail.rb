e02735428b4e5f7f266602bf65e45c4bef0d0f72
vishal <design@indibasolutions.com>
2018-04-06 15:05:13 +0530

    pull

--- db/migrate/20180123071508_add_col_to_joining_detail.rb
+++
@@ -1,5 +0,0 @@
-class AddColToJoiningDetail < ActiveRecord::Migration
-  def change
-    add_column :joining_details, :contact_library, :boolean
-  end
-end
--- db/migrate/20180131100047_change_col_in_to_leave_record.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColInToLeaveRecord < ActiveRecord::Migration
-  def change
-  	
-change_column :leave_records, :count, :decimal, precision: 15, scale: 2
-  end
-end

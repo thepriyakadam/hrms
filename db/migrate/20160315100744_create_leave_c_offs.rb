a2bb5f0e156884ed867c92df6c596456de3ffd08
admin <design@indibasolutions.com>
2018-06-18 19:08:31 +0530

    pull

---
+++ db/migrate/20160315100744_create_leave_c_offs.rb
@@ -0,0 +1,22 @@
+class CreateLeaveCOffs < ActiveRecord::Migration
+  def change
+    create_table :leave_c_offs do |t|
+      t.references :employee, index: true, foreign_key: true
+      t.date :c_off_date
+      t.string :c_off_type
+      t.integer :c_off_expire_day, default: 60
+      t.boolean :expiry_status
+      t.boolean :is_taken, default: false
+      t.date :expiry_date
+      t.decimal :leave_count, precision: 3, scale: 1
+      t.boolean :is_expire
+      t.boolean :status
+      t.integer :c_off_expire_day, default: false
+      t.string :current_status
+      t.date :taken_date
+      t.text :comment
+
+      t.timestamps null: false
+    end
+  end
+end

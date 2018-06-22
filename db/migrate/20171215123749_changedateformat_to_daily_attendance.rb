60143665f9988fbb435fa083b739ca81968d5e0c
vinod <design@indibasolutions.com>
2018-06-22 10:55:12 +0530

    old migrations removed

--- db/migrate/20171215123749_changedateformat_to_daily_attendance.rb
+++
@@ -1,5 +0,0 @@
-class ChangedateformatToDailyAttendance < ActiveRecord::Migration
-  def change
-	change_column :daily_attendances, :time, :datetime
-  end
-end
--- db/migrate/20180502060522_add_col_to_employee_transfer.rb
+++
@@ -1,8 +0,0 @@
-class AddColToEmployeeTransfer < ActiveRecord::Migration
-  def change
-    add_column :employee_transfers, :from, :date
-    add_column :employee_transfers, :to, :date
-    add_column :transfer_histories, :from, :date
-    add_column :transfer_histories, :to, :date
-  end
-end

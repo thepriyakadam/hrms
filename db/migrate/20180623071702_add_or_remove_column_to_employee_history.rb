beeca8b2784e86d8db63c3a4f0cdc626048a96c8
IBSL <design@indibasolutions.com>
2018-06-25 09:47:50 +0530

    fixed untracked files

--- db/migrate/20180623071702_add_or_remove_column_to_employee_history.rb
+++
@@ -1,8 +0,0 @@
-class AddOrRemoveColumnToEmployeeHistory < ActiveRecord::Migration
-  def change
-  	remove_column :employee_location_histories, :date_time
-
-  	add_column :employee_location_histories, :date, :date
-  	add_column :employee_location_histories, :time, :time
-  end
-end

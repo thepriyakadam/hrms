6413a65c07956e9eb8200fa981c3e6714bae2c70
vinod <design@indibasolutions.com>
2018-06-25 09:34:55 +0530

    new changes

---
+++ db/migrate/20180623071702_add_or_remove_column_to_employee_history.rb
@@ -0,0 +1,8 @@
+class AddOrRemoveColumnToEmployeeHistory < ActiveRecord::Migration
+  def change
+  	remove_column :employee_location_histories, :date_time
+
+  	add_column :employee_location_histories, :date, :date
+  	add_column :employee_location_histories, :time, :time
+  end
+end

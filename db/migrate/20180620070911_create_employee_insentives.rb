32604ce402060ea6a53e35c727e75d29dda969a3
Ubuntu <ubuntu@ip-172-31-2-64.ap-southeast-1.compute.internal>
2018-07-12 09:07:14 +0000

    pull

--- db/migrate/20180620070911_create_employee_insentives.rb
+++ db/migrate/20180620070911_create_employee_insentives.rb
@@ -1,13 +1,13 @@
 class CreateEmployeeInsentives < ActiveRecord::Migration
   def change
-    create_table :employee_insentives do |t|
-      t.references :employee, index: true, foreign_key: true
-      t.date :date
-      t.decimal :amount
-      t.boolean :status
-      t.text :description
+#    create_table :employee_insentives do |t|
+ #     t.references :employee, index: true, foreign_key: true
+  #    t.date :date
+   #   t.decimal :amount
+    #  t.boolean :status
+     # t.text :description
 
-      t.timestamps null: false
-    end
+      #t.timestamps null: false
+#    end
   end
 end
--- db/migrate/20180706094527_create_food_options.rb
+++ db/migrate/20180706094527_create_food_options.rb
@@ -1,15 +1,15 @@
 class CreateFoodOptions < ActiveRecord::Migration
   def change
-    create_table :food_options do |t|
-      t.references :travel_option, index: true, foreign_key: true
-      t.string :code
-      t.string :name
-      t.text :description
-      t.decimal :amount, :precision => 15, :scale => 10
-      t.boolean :is_optional
-      t.boolean :status
+#    create_table :food_options do |t|
+ #     t.references :travel_option, index: true, foreign_key: true
+  #    t.string :code
+   #   t.string :name
+    #  t.text :description
+     # t.decimal :amount, :precision => 15, :scale => 10
+     # t.boolean :is_optional
+     # t.boolean :status
 
-      t.timestamps null: false
-    end
+      #t.timestamps null: false
+   # end
   end
 end

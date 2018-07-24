032205ac4fff9a5cbbc2ce3fba8eac4f913f9e14
root <design@indibasolutions.com>
2018-07-24 18:56:37 +0530

    pull

--- db/migrate/20180614060121_change_column_name_transaction_records.rb
+++ db/migrate/20180614060121_change_column_name_transaction_records.rb
@@ -1,5 +1,5 @@
 class ChangeColumnNameTransactionRecords < ActiveRecord::Migration
   def change
-  	# rename_column :transaction_records, :type, :transaction_type
+  	 rename_column :transaction_records, :type, :transaction_type
   end
 end
--- db/migrate/20180707053515_add_column_to_transaction_records.rb
+++ db/migrate/20180707053515_add_column_to_transaction_records.rb
@@ -1,5 +1,5 @@
 class AddColumnToTransactionRecords < ActiveRecord::Migration
   def change
-    # add_column :transaction_records, :reminder_to, :date
+     add_column :transaction_records, :reminder_to, :date
   end
 end

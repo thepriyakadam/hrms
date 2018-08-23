39350d5e4d125540886d8036e61ed34c2a2e1a8c
Linux Lab <linuxlab@hrms.uttamenergy.local>
2018-08-23 15:54:21 +0530

    new chnages and documment

--- app/controllers/goal_bunches_controller.rb
+++ app/controllers/goal_bunches_controller.rb
@@ -163,7 +163,7 @@ class GoalBunchesController < ApplicationController
     else
       flash[:alert] = "Goal weightage sum should be 100"
     end 
-    if @employee.id == current_user.employee_id
+    if @employee.manager_id == current_user.employee_id
       redirect_to goal_period_list_goal_bunches_path(period_id: @period.id)
     else
       redirect_to admin_level_goal_approval_goal_bunches_path(period_id: @period.id)
@@ -176,7 +176,12 @@ class GoalBunchesController < ApplicationController
     @goal_bunch = GoalBunch.find_by(id: @goal_bunch_id)
     @goal_bunch.update(goal_approval: false,goal_confirm: false)
       flash[:alert] = "Goal Rejected Successfully"
+      #redirect_to goal_period_list_goal_bunches_path(period_id: @period.id)
+    if @employee.manager_id == current_user.employee_id
       redirect_to goal_period_list_goal_bunches_path(period_id: @period.id)
+    else
+      redirect_to admin_level_goal_approval_goal_bunches_path(period_id: @period.id)
+    end
   end
 
   def create_multiple_bunch

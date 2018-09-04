34ab20fa9db1424a8542b47923853812bee514c5
vinod <design@indibasolutions.com>
2018-09-04 15:54:12 +0530

    migration

--- db/migrate/20180703112751_add_remove_col_in_travel_requests.rb
+++ db/migrate/20180703112751_add_remove_col_in_travel_requests.rb
@@ -1,6 +1,6 @@
 class AddRemoveColInTravelRequests < ActiveRecord::Migration
   def change
-  	# remove_reference :travel_requests, :reporting_master, index: true, foreign_key: true
-    # add_reference :travel_requests, :reporting_master
+  	remove_reference :travel_requests, :reporting_master, index: true, foreign_key: true
+    add_reference :travel_requests, :reporting_master
   end
 end

867bd69d11d0215c23eeb513c68ea692b2e14dfd
dev009 <yo89ku@gmail.com>
2018-10-08 18:10:15 +0530

    push

--- config/routes.rb
+++ config/routes.rb
@@ -1312,7 +1312,7 @@ end
   resources :goal_ratings do
     collection do
       get :period_for_status
-      post :managerwise_status
+      get :managerwise_status
       get :periodwise_goal_set
       post :periodwise_goal_list
       post :set_goal_periodwise

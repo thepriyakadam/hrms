d8a5733055fbd62133dd21ab0dfe9b7d8b4e0056
dev009 <yo89ku@gmail.com>
2018-11-12 14:39:13 +0530

    new chnges

--- db/migrate/20180614045932_create_transaction_records.rb
+++ db/migrate/20180614045932_create_transaction_records.rb
@@ -1,4 +1,5 @@
 class CreateTransactionRecords < ActiveRecord::Migration
+<<<<<<< HEAD
   # def change
   #   create_table :transaction_records do |t|
   #     # t.references :compliance_type, index: true, foreign_key: true
@@ -16,4 +17,23 @@ class CreateTransactionRecords < ActiveRecord::Migration
   #     t.timestamps null: false
   #   end
   # end
+=======
+  def change
+    create_table :transaction_records do |t|
+      t.references :compliance_type, index: true, foreign_key: true
+      t.string :type
+      t.date :submission_date
+      t.date :reminder_Date
+      t.string :previous_days
+      t.string :after_days
+      t.string :email_1
+      t.string :email_2
+      t.string :email_3
+      t.string :status
+      t.boolean :is_confirm
+
+      t.timestamps null: false
+    end
+  end
+>>>>>>> 485202c366319d4204acbcd3498554839acbd212
 end

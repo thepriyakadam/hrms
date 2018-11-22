0152a2cfc503538b10cc3d5144ef4314c5959a83
vinod <design@indibasolutions.com>
2018-11-22 15:53:37 +0530

    new chnges

--- db/migrate/20181122095234_add_latitude_longitude_place_comment_to_daily_attendances.rb
+++
@@ -1,8 +0,0 @@
-class AddLatitudeLongitudePlaceCommentToDailyAttendances < ActiveRecord::Migration
-  def change
-    add_column :daily_attendances, :latitude, :float
-    add_column :daily_attendances, :longitude, :float
-    add_column :daily_attendances, :place, :string
-    # add_column :daily_attendances, :comment, :string
-  end
-end
---
+++ db/migrate/20181122101851_add_holiday_to_employee_attendances.rb
@@ -0,0 +1,5 @@
+class AddHolidayToEmployeeAttendances < ActiveRecord::Migration
+  def change
+    add_reference :employee_attendances, :holiday, index: true, foreign_key: true
+  end
+end

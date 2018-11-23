9f684fd4c9662f50e73f71a15e4752c98cfb266c
Indiba1 <design@indibasolutions.com>
2018-11-23 10:39:43 +0530

    pull

--- db/migrate/20180910121243_add_reff_to_shift_employees.rb
+++
@@ -1,6 +0,0 @@
-class AddReffToShiftEmployees < ActiveRecord::Migration
-  def change
-    add_reference :shift_employees, :shift_time, index: true, foreign_key: true
-    add_reference :shift_employees, :created_by
-  end
-end

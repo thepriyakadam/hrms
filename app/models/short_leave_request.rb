d7762da0dbfb6cb0373ff54bf4a42f70dc97edd1
dev009 <yo89ku@gmail.com>
2019-01-03 19:16:21 +0530

    push

--- app/models/short_leave_request.rb
+++ app/models/short_leave_request.rb
@@ -1,5 +1,6 @@
 class ShortLeaveRequest < ActiveRecord::Base
   belongs_to :employee
+  belongs_to :leav_category
   belongs_to :manager, class_name: 'Employee'
   has_many :short_leave_approvals
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
       get :approve
       get :reject
       get :view_request
+      get :short_leave_process
+      post :show_list_for_process
+      post :process_short_leave
     end
   end
   resources :c_off_cash_masters

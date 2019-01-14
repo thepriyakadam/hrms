ee251dd3c6858365e96e7e893e876c3a51bf243d
dev009 <yo89ku@gmail.com>
2019-01-14 10:46:43 +0530

    premier

--- db/migrate/20180111084545_add_col_to_leav_category.rb
+++ db/migrate/20180111084545_add_col_to_leav_category.rb
@@ -1,5 +1,5 @@
 class AddColToLeavCategory < ActiveRecord::Migration
-  def change
-    add_column :leav_categories, :pre_request, :boolean
-  end
+  # def change
+  #   add_column :leav_categories, :pre_request, :boolean
+  # end
 end

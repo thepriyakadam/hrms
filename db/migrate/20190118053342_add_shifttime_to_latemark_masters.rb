32951b2e398185e5bd785b933b5d9f302d7ba707
IBSL <design@indibasolutions.com>
2019-01-22 15:48:51 +0530

    Pull From Premier

--- db/migrate/20190118053342_add_shifttime_to_latemark_masters.rb
+++ db/migrate/20190118053342_add_shifttime_to_latemark_masters.rb
@@ -1,5 +1,5 @@
 class AddShifttimeToLatemarkMasters < ActiveRecord::Migration
   def change
-    add_reference :latemark_masters, :shift_time, index: true, foreign_key: true
+   # add_reference :latemark_masters, :shift_time, index: true, foreign_key: true
   end
 end
